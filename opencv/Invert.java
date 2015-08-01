ca692522efb43a3a23a5a080d341bb5f1af9c485
tanaka0079 <yadoran001@yahoo.co.jp>
2015-08-01 19:06:53 +0900

    Update and rename BackgroundSubtraction.pde to Invert.java

---
+++ opencv/Invert.java
@@ -0,0 +1,17 @@
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
+
+public class Invert{
+	public static void main(String[] args){
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat im = Imgcodecs.imread("test.jpg");	// 入力画像の取得
+		Core.bitwise_not(im, im); 				// 色反転(Not演算)
+		Imgcodecs.imwrite("test2.jpg", im);			// 出力画像の保存
+	}
+}
