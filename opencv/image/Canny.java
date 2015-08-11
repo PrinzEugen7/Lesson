b944fc645777a67e18ae71159575cd9a2083e69b
tanaka0079 <yadoran001@yahoo.co.jp>
2015-08-11 22:51:23 +0900

    Update Canny.java

--- opencv/image/Canny.java
+++ opencv/image/Canny.java
@@ -7,10 +7,10 @@ import org.opencv.imgproc.Imgproc;
 public class CvTest {
 	public static void main(String args[]){
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat im = Imgcodecs.imread("test.jpg");					    // 入力画像の取得
+		Mat im = Imgcodecs.imread("test.jpg");			// 入力画像の取得
 		Mat gray = new Mat();           
-		Imgproc.cvtColor(im, gray, Imgproc.COLOR_RGB2GRAY); // グレースケール変換
-		Imgproc.Canny(gray, gray, 400, 500, 5, true);       // Cannyアルゴリズムで輪郭検出
-		Imgcodecs.imwrite("test2.jpg", gray);					      // エッジ画像の出力
+		Imgproc.cvtColor(im, gray, Imgproc.COLOR_RGB2GRAY); 	// グレースケール変換
+		Imgproc.Canny(gray, gray, 400, 500, 5, true);       	// Cannyアルゴリズムで輪郭検出
+		Imgcodecs.imwrite("test2.jpg", gray);			// エッジ画像の出力
 	}
 }
