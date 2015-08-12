910a2c966affe93271b63ddbfbfc6a816b734bb1
tanaka0079 <yadoran001@yahoo.co.jp>
2015-08-12 21:20:57 +0900

    Update EqualizeHistgram.java

--- opencv/image/EqualizeHistgram.java
+++ opencv/image/EqualizeHistgram.java
@@ -7,10 +7,10 @@ import org.opencv.imgproc.Imgproc;
 public class CvTest {
 	public static void main(String args[]){
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat im = Imgcodecs.imread("test.jpg");				// 入力画像の取得
+		Mat im = Imgcodecs.imread("test.jpg");			// 入力画像の取得
 		Mat gray = new Mat();
-		Imgproc.cvtColor(im, gray, Imgproc.COLOR_RGB2GRAY); // 画像のグレースケール変換
-    Imgproc.equalizeHist(gray, gray);                   	// グレースケール画像のヒストグラムを平坦化
-		Imgcodecs.imwrite("test2.jpg", gray);				// 画像の出力
+		Imgproc.cvtColor(im, gray, Imgproc.COLOR_RGB2GRAY)	// 画像のグレースケール変換
+    		Imgproc.equalizeHist(gray, gray)			// グレースケール画像のヒストグラムを平坦化
+		Imgcodecs.imwrite("test2.jpg", gray);			// 画像の出力
 	}
 }
