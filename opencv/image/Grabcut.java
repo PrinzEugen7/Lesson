722f026de9bf803b0096287123d2721106713300
tanaka0079 <yadoran001@yahoo.co.jp>
2015-08-12 11:24:36 +0900

    グラフカットで前景抽出(Java+OpenCV3)

--- opencv/image/Grabcut.java
+++ opencv/image/Grabcut.java
@@ -9,17 +9,17 @@ import org.opencv.imgproc.Imgproc;
 
 public class CvTest {
 	public static void main(String args[]){
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat im = Imgcodecs.imread("test.jpg");					// 入力画像の取得			// 入力画像の取得
-        Mat result = new Mat();
-        Mat bgModel = new Mat();
-        Mat fgModel = new Mat();
-        Rect rect = new Rect(10, 10,250,290);
-        Mat source = new Mat(1, 1, CvType.CV_8U, new Scalar(3));
-        Imgproc.grabCut(im, result, rect, bgModel, fgModel, 1, 0);
-        Core.compare(result, source,result, Core.CMP_EQ);
-        Mat fg = new Mat(im.size(), CvType.CV_8UC1, new Scalar(0, 0, 0));
-        im.copyTo(fg, result);
-		Imgcodecs.imwrite("test2.jpg", fg);					// 画像の出力
+	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	Mat im = Imgcodecs.imread("test.jpg");					// 入力画像の取得			// 入力画像の取得
+        Mat mask = new Mat();							// マスク画像用
+        Mat bgModel = new Mat();						// 背景モデル用
+        Mat fgModel = new Mat();						// 前景モデル用
+        Rect rect = new Rect(10, 10,250,290);					// 大まかな前景と背景の境目(矩形)
+        Mat source = new Mat(1, 1, CvType.CV_8U, new Scalar(3));		
+        Imgproc.grabCut(im, mask, rect, bgModel, fgModel, 1, 0);		// グラフカットで前景と背景を分離
+        Core.compare(mask, source, mask, Core.CMP_EQ);
+        Mat fg = new Mat(im.size(), CvType.CV_8UC1, new Scalar(0, 0, 0));	// 前景画像用
+        im.copyTo(fg, mask);							// 前景画像の作成
+	Imgcodecs.imwrite("test2.jpg", fg);					// 画像の出力
 	}
 }
