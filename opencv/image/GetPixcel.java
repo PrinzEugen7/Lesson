3175aea89886f3964ea6073d05cb241ae723bbe2
tanaka0079 <yadoran001@yahoo.co.jp>
2015-08-12 21:48:30 +0900

    Update GetPixcel.java

--- opencv/image/GetPixcel.java
+++ opencv/image/GetPixcel.java
@@ -7,15 +7,15 @@ import org.opencv.imgproc.Imgproc;
 public class CvTest {
 	public static void main(String args[]){
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat im = Imgcodecs.imread("test.jpg");					    // 入力画像の取得
-		Imgproc.cvtColor(im, gray, Imgproc.COLOR_RGB2GRAY); // 画像のグレースケール変換
+		Mat im = Imgcodecs.imread("test.jpg");			// 入力画像の取得
+		Imgproc.cvtColor(im, gray, Imgproc.COLOR_RGB2GRAY); 	// 画像のグレースケール変換
 		// カラー画像の画素にアクセスして画素値(R,G,B)を取得
 		double[] data = new double[3]; 
-		data = im.get(100, 200);                            // (100,200)の画素値を取得
+		data = im.get(100, 200);                            	// (100,200)の画素値を取得
 		// グレースケール画像の画素にアクセスして画素値を取得
 		Mat gray = new Mat();
 		double[] data2 = new double[1]; 
-		data2 = gray.get(100, 200);                         // (100,200)の画素値を取得
+		data2 = gray.get(100, 200);                         	// (100,200)の画素値を取得
 		//取得した画素値をコンソールに表示
 		System.out.println("Blue：" + data[0]);
 		System.out.println("Green：" + data[1]);
