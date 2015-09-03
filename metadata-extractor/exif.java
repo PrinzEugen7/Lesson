65dcf09df18f9da1e4e5d7a7ba8c7429636124bd
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-04 01:56:43 +0900

    Create exif.java

---
+++ metadata-extractor/exif.java
@@ -0,0 +1,22 @@
+package info.cvtest;
+import java.io.File;
+import java.io.IOException;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+public class CvTest {
+	public static void main(String args[]) throws ImageProcessingException, IOException 
+	{
+	      File jpegFile = new File("test.jpg");
+	      Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);	// 画像からExif情報を抽出
+	      // Exif情報を全て表示
+	      for (Directory directory : metadata.getDirectories()) {
+	          for (Tag tag : directory.getTags()) {
+	              System.out.println(tag);
+	          }
+	      }
+	    }
+}
