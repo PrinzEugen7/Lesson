d7532987ad7647a26e7895342f1893b61e0641f0
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-04 16:44:10 +0900

    Update dispImg.java

--- swing/image/dispImg.java
+++ swing/image/dispImg.java
@@ -5,13 +5,13 @@ import javax.swing.JLabel;
 public class CvTest {
 	public static void main(String args[]) 
 	{
-        JFrame frame = new JFrame("画像表示");			// ウィンドウのタイトル名
+        JFrame frame = new JFrame("画像表示");				// ウィンドウのタイトル名
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(500, 400);									// ウィンドウのサイズ
-        ImageIcon icon = new ImageIcon("test.jpg");        // 画像を取得してアイコンにする
-        JLabel label = new JLabel(icon);							// アイコンを表示するラベルを作成
-        frame.getContentPane().add(label);						// ウィンドウのContentPaneにラベルを追加
-        frame.setVisible(true); 										// ウィンドウを表示
+        frame.setSize(500, 400);					// ウィンドウのサイズ
+        ImageIcon icon = new ImageIcon("test.jpg");        		// 画像を取得してアイコンにする
+        JLabel label = new JLabel(icon);				// アイコンを表示するラベルを作成
+        frame.getContentPane().add(label);				// ウィンドウのContentPaneにラベルを追加
+        frame.setVisible(true)						// ウィンドウを表示
 	}
 	  
 }
