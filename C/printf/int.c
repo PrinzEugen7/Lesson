738ede2f9255a2c448ab1f60e2b8bf9bf9ffe5e8
PrinzEugen7 <nsi05162@pooae.com>
2016-12-30 12:10:27 +0900

    Update int.c

--- C/printf/int.c
+++ C/printf/int.c
@@ -6,7 +6,7 @@ int main(void)
   int y = 20;  // 変数yの宣言・初期化
   
   printf("%d\n", x);  // 変数の中身のみ表示
-  printf("xは%x\n", x); // 任意の文字列と変数の中身を表示
+  printf("xは%d\n", x); // 任意の文字列と変数の中身を表示
   printf("xは%d yは%d", x, y); // 任意の文字列と複数の変数の中身を表示
   
   return 0;
