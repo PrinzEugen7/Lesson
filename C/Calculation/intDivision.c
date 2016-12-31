cd95330d0b26e5ad06eac1bd5ce8a5539233941d
PrinzEugen7 <nsi05162@pooae.com>
2016-12-31 14:49:29 +0900

    Create intDivision.c

---
+++ C/Calculation/intDivision.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int x = 5;  // 変数xの宣言・初期化
+  int y = 3;  // 変数yの宣言・初期化
+  int z = 0; 
+  
+  z = 5 / 3;  // 5を3で割る
+  
+  printf("%c\n", z);  // 計算結果を表示
+  
+  return 0;
+}
