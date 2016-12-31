2f76693df93faa9cfc1a1de5e496ace64e38ce6f
PrinzEugen7 <nsi05162@pooae.com>
2016-12-31 15:53:33 +0900

    Create int2double.c

---
+++ C/Calculation/int2double.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int x = 5;  // 変数xの宣言・初期化
+  int y = 3;  // 変数yの宣言・初期化
+  int z = 0; 
+  
+  z = (double) x / y;  // 5を3で割る
+  
+  printf("%d\n", z);  // 計算結果を表示
+  
+  return 0;
+}
