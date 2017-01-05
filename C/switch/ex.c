0f3440a11adaad16da3a6268dc648fa48e23a300
PrinzEugen7 <nsi05162@pooae.com>
2017-01-05 17:17:36 +0900

    Create ex.c

---
+++ C/switch/ex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i = 1;
+
+  switch (i)
+  {
+    case 1:
+    printf("iは1です。\n");
+
+    case 2:
+    printf("iは2です。\n");
+    
+    case 3:
+    printf("iは3です。\n");
+    
+    default:
+    printf("iは1～3以外です。");
+  }
+}
