f42491c68a5fbb02e4ebabe6ce8af79c3f848680
PrinzEugen7 <nsi05162@pooae.com>
2017-01-08 23:59:57 +0900

    Update array.c

--- C/Structure/array.c
+++ C/Structure/array.c
@@ -5,15 +5,15 @@ int main()
     // 変数の宣言
     int i = 0;
     
-    // 構造体配列の定義
-    typedef struct score[3]{
+    // 構造体の定義
+    typedef struct score{
         int english;      // 英語のと得点
         int math;         // 数学の得点
         char name[10];    // 受験者氏名
     }SCORE;
     
-    // 構造体の宣言・初期化
-    SCORE scores = {
+    // 構造体配列の宣言・初期化
+    SCORE scores[3] = {
        {80, 91, "Ayase"},
        {84, 81, "Kirino"},
        {72, 71, "Kuroneko"}
@@ -24,19 +24,19 @@ int main()
         printf("受験者氏名：%s\n", scores[i].name);
         printf("英語の得点：%d\n", scores[i].english);
         printf("数学の得点：%d\n", scores[i].math);
-        printf("------------------");
+        printf("------------------\n");
     }
     
     // メンバにデータを代入（更新）
-    scores.english[0] = 88;
-    scores.math[1] = 90;
+    scores[0].english = 88;
+    scores[1].math = 90;
     
     // 各メンバのデータを表示
     for(i = 0; i < 3; i++){
         printf("受験者氏名：%s\n", scores[i].name);
         printf("英語の得点：%d\n", scores[i].english);
         printf("数学の得点：%d\n", scores[i].math);
-        printf("------------------");
+        printf("------------------\n");
     }
     
     return 0;
