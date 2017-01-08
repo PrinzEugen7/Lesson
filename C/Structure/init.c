0c28d6ebf312dd26e60eb08726a28b9be28f0a9d
PrinzEugen7 <nsi05162@pooae.com>
2017-01-08 19:05:51 +0900

    Update init.c

--- C/Structure/init.c
+++ C/Structure/init.c
@@ -4,9 +4,9 @@ int main()
 {
     // 構造体の定義
     typedef struct score{
-        int english; 　　　// 英語のと得点
-        int math;    　　　// 数学の得点
-        char name[10];  // 受験者氏名
+        int english;      // 英語のと得点
+        int math;         // 数学の得点
+        char name[10];    // 受験者氏名
     }SCORE;
     
     // 構造体の宣言・初期化
