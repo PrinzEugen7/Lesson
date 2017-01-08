ca4118940faf88725ba23796d2541f391c7afabd
PrinzEugen7 <nsi05162@pooae.com>
2017-01-08 15:00:15 +0900

    Update argument.c

--- C/UserFunction/argument.c
+++ C/UserFunction/argument.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-// ユーザ関数の宣言
-int func();  // ユーザ関数を宣言(int型)
+// ユーザ関数を宣言・定義
+int func(int x1, int x2)
+{
+  
+  x1 = x1 * 10;
+  x2 = x2 * 10;
+  
+  printf("x1=%d, x2=%d \n", x1, x2);
+ 
+  return 0;  // 型があるので戻り値が必要
+}
 
 // メイン関数
 int main()
@@ -13,15 +22,4 @@ int main()
   printf("x=%d, y=%d \n", x, y);
   
   return 0;
-}
-// ユーザ関数を定義
-int func1(int x1, int x2)
-{
-  
-  x1 = x1 * 10;
-  x2 = x2 * 10;
-  
-  printf("x1=%d, x2=%d \n", x1, x2);
- 
-  return 0;  // 型があるので戻り値が必要
 }
