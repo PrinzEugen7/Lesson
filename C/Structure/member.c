e7bbdb5c43b314d1c6a993d3fee86a0293e49484
PrinzEugen7 <nsi05162@pooae.com>
2017-01-08 23:40:27 +0900

    Create member.c

---
+++ C/Structure/member.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    // 構造体の定義
+    typedef struct score{
+        int english;      // 英語のと得点
+        int math;         // 数学の得点
+        char name[10];    // 受験者氏名
+    }SCORE;
+    
+    // 構造体の宣言・初期化
+    SCORE scores = {80, 91, "Ayase"};
+    
+    // 各メンバのデータを表示
+    printf("英語の得点：%d\n", scores.english);
+    printf("数学の得点：%d\n", scores.math);
+    printf("受験者氏名：%s\n", scores.name);
+    
+    // メンバにデータを代入（更新）
+    scores.english = 88;
+    scores.math = 90;
+    scores.name = "kirino";
+    
+    // 各メンバのデータを表示
+    printf("英語の得点：%d\n", scores.english);
+    printf("数学の得点：%d\n", scores.math);
+    printf("受験者氏名：%s\n", scores.name);
+    
+    return 0;
+}
