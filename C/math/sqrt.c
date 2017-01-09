4a910a23d781e7cb92a624c0b710e84bf6498838
PrinzEugen7 <nsi05162@pooae.com>
2017-01-09 12:36:05 +0900

    Create sqrt.c

---
+++ C/math/sqrt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double x = 2.0;
+    double y = 0; 
+    // xの平方根を計算
+    y = sqrt(x);
+    // 計算結果の表示
+    printf("y=%f", y);
+  
+    return 0;
+}
