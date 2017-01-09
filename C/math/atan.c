20705db030c9968ae56ffe1addc645620ddff56b
PrinzEugen7 <nsi05162@pooae.com>
2017-01-09 12:31:27 +0900

    Create atan.c

---
+++ C/math/atan.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+  
+int main(void)
+{
+  double x = 0;
+  double y = 1;
+  
+  x = atan(y);        // 逆正弦の計算
+  
+  printf("y=%f", x);  // 計算結果の表示
+  
+  return 0;
+}
