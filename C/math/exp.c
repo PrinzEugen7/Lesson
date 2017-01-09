fede54e86fcd84e251d1201660e51b86c848b269
PrinzEugen7 <nsi05162@pooae.com>
2017-01-09 12:35:12 +0900

    Create exp.c

---
+++ C/math/exp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+  
+int main(void)
+{
+  double x = 2.0;
+  double y = 0; 
+  // 指数関数の計算
+  y = exp(x);
+  // 計算結果の表示
+  printf("y=%f", y);
+  
+  return 0;
+}
