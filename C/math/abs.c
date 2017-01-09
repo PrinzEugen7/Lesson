9f89981d952f6bdc839ac620f6af5efb572fb578
PrinzEugen7 <nsi05162@pooae.com>
2017-01-09 12:37:39 +0900

    Create abs.c

---
+++ C/math/abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+  
+int main(void)
+{
+  int x = -5;
+  int y;
+  
+  y = abs(x);         // xの絶対値を計算
+  
+  printf("y=%d", y);  // 計算結果の表示
+  
+  return 0;
+}
