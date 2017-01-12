82292ceea71470c3218fd6323e60bb555940260e
PrinzEugen7 <nsi05162@pooae.com>
2017-01-12 13:30:45 +0900

    Create sum.py

---
+++ Python/List/sum.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+def main():
+    data = [1,2,3,4,5]  # 配列の生成
+    dsum = sum(data)    # 合計(総和)を計算
+    print(dsum)         # 計算結果を表示
+
+if __name__ == '__main__':
+    main()
