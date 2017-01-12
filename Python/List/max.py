6abab2053082b9a370ed998284e70be5254f8bc1
PrinzEugen7 <nsi05162@pooae.com>
2017-01-12 13:31:34 +0900

    Update max.py

--- Python/List/max.py
+++ Python/List/max.py
@@ -2,7 +2,7 @@
 def main():
     data = [1,2,3,4,5]  # 配列の生成
     dmax = max(data)    # 最大値を計算
-    print dmax          # 計算結果を表示
+    print(dmax)         # 計算結果を表示
 
 if __name__ == '__main__':
     main()
