c23c740903e9a46307c32c3decc048838e0b46f7
PrinzEugen7 <nsi05162@pooae.com>
2017-01-17 12:52:31 +0900

    Create class_variable_get.py

---
+++ Python/Class/class_variable_get.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# クラスの定義
+class MyClass():
+    x = 10
+
+def main():
+    # クラス変数の値を取得
+    x = MyClass.x
+
+    # クラス変数の中身を表示
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
