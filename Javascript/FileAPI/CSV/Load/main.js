83a3d60544ea8e8c26144f301c10e41f955d4cbd
PrinzEugen7 <nsi05162@pooae.com>
2017-01-21 20:52:02 +0900

    Update main.js

--- Javascript/FileAPI/CSV/Load/main.js
+++ Javascript/FileAPI/CSV/Load/main.js
@@ -1,43 +1,33 @@
-function main(){
-	var result = document.getElementById('result');
-	var select = document.getElementById('select');
-
-	// ファイルが選択されたとき
-	select.addEventListener('change', function(e) {
-		// 選択されたファイルの情報を取得
-		var fileData = e.target.files[0];
-
-		var loader = new FileReader();
-		// ロード失敗時
-		loader.onerror = function() {
-			alert("ファイルを正常に取得できませんでした。");
-		}
-		// ロード成功時
-		loader.onload = function() {
-			// 行単位で配列にする
-			var lineArr = loader.result.split("\n");
-			// 行と列の二次元配列にする
-			var itemArr = [];
-			for (var i = 0; i < lineArr.length; i++) {
-				itemArr[i] = lineArr[i].split(",");
-			}
-
-			// tableで出力
-			var insert = '<table>';
-			for (var i = 0; i < itemArr.length; i++) {
-				insert += '<tr>';
-				for (var j = 0; j < itemArr[i].length; j++) {
-					insert += '<td>';
-					insert += itemArr[i][j];
-					insert += '</td>';
-				}
-				insert += '</tr>';
-			}
-			insert += '</table>';
-			result.innerHTML = insert;
-		}
-
+// アップロード時の処理
+function upload(evt) {
+	if (!isFileUpload()) {
+		alert("エラー：FileAPI非対応のブラウザです。");
+	} else {
+		var data = null;
+		// 選択されたファイル情報を取得
+		var file = evt.target.files[0];
+		var reader = new FileReader();
 		// ファイル読み取りを実行
-		loader.readAsText(fileData);
-	}, false);
+		reader.reader.readAsText(file);
+		// ファイルの内容を表示
+		reader.onload = function(event) {
+			var result = event.target.result;
+			alert(result)
+		};
+		reader.onerror = function() {
+			alert("エラー：ファイルをロードできません。");
+		};
+	}
 }
+
+// ファイルアップロード判定
+function isFileUpload() {
+	   var isCompatible = false;
+	   if (window.File && window.FileReader && window.FileList && window.Blob) {
+	   isCompatible = true;
+	   }
+	   return isCompatible;
+}
+
+var loadBtn = document.querySelector("#loadBtn");
+loadBtn.addEventListener('change', upload, false);
