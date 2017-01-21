673cfa5a0ae7a7da0abc4011c8e799e86bf33a26
PrinzEugen7 <nsi05162@pooae.com>
2017-01-21 23:58:23 +0900

    Update main.js

--- Javascript/Basic/String/Connect/main.js
+++ Javascript/Basic/String/Connect/main.js
@@ -3,7 +3,8 @@ function main()
     var str1 = "<script type=\"text/javascript\" src=\"http://ext.nicovideo.jp/thumb_watch/"
     var str2 = "sm29483618"
     var str3 = "\"></script>"
-	
-	var str = str1 + str2 + str3;
-	alert(str);
+    // 文字列の連結
+    var str = str1 + str2 + str3;
+    // 結果表示
+    alert(str);
 }
