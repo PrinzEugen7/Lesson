f5432176cd3e671d153be1e8d3133b8a038353a2
PrinzEugen7 <nsi05162@pooae.com>
2017-01-21 23:48:32 +0900

    Create main.js

---
+++ Javascript/Basic/String/Connect/main.js
@@ -0,0 +1,9 @@
+function main()
+{
+    var str1 = "<script type=\"text/javascript\" src=\"http://ext.nicovideo.jp/thumb_watch/"
+    var str2 = "sm29483618"
+    var str3 = "\"></script>"
+	
+	var str = str1 + str2 + str3;
+	alert(str);
+}
