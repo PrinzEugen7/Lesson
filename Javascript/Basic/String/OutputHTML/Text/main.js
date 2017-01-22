0c399d1407c182aa50a826a9c4cd82a61b0584a7
PrinzEugen7 <nsi05162@pooae.com>
2017-01-22 17:58:53 +0900

    Create main.js

---
+++ Javascript/Basic/String/OutputHTML/Text/main.js
@@ -0,0 +1,7 @@
+function main()
+{
+    // 文字列の宣言・初期化
+    var str = "<script type=\"text/javascript\" src=\"http://ext.nicovideo.jp/thumb_watch/sm29483618\"></script>"
+    // テキストのみ書き換え
+    document.getElementById("nicovideo").textContent=str;
+}
