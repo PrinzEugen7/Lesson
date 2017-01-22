a1bb9626621ea0bd4f889c930e3f7423137e8af2
PrinzEugen7 <nsi05162@pooae.com>
2017-01-22 18:03:16 +0900

    Update main.js

--- Javascript/Basic/String/OutputHTML/Tag/main.js
+++ Javascript/Basic/String/OutputHTML/Tag/main.js
@@ -1,7 +1,7 @@
 function main()
 {
     // 文字列の宣言・初期化
-    var str = "<script type=\"text/javascript\" src=\"http://ext.nicovideo.jp/thumb_watch/sm29483618\"></script>"
+    var str = "<script type=\"text/javascript\" src=\"http://ext.nicovideo.jp/thumb_watch/sm29483618\"></script>";
     // HTMLタグも含めて書き換え
-	  document.getElementById("nicovideo")..innerHTML=str;
+	document.getElementById("nicovideo").innerHTML = str;
 }
