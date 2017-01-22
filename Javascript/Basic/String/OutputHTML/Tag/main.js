5523fbac88aa694321eebcb3507ac454ae663625
PrinzEugen7 <nsi05162@pooae.com>
2017-01-22 18:00:47 +0900

    Create main.js

---
+++ Javascript/Basic/String/OutputHTML/Tag/main.js
@@ -0,0 +1,7 @@
+function main()
+{
+    // 文字列の宣言・初期化
+    var str = "<script type=\"text/javascript\" src=\"http://ext.nicovideo.jp/thumb_watch/sm29483618\"></script>"
+    // HTMLタグも含めて書き換え
+	  document.getElementById("nicovideo")..innerHTML=str;
+}
