7b587a9a2dc0f20dd37033f9234bfa798c122bd5
PrinzEugen7 <nsi05162@pooae.com>
2017-02-02 22:13:31 +0900

    Create main.js

---
+++ Javascript/if/ex2/main.js
@@ -0,0 +1,18 @@
+function main(){
+
+      var x = 1;
+	  
+      if(x > 3){
+        alert("xは3より大きい\n");
+      }
+      else if(x > 2){
+        alert("xは2より大きい\n");
+      }
+      else if(x > 1){
+        alert("xは1より大きい\n");
+      }
+      else {
+        alert("xは1以下だ\n");
+      }
+
+}
