29ef487b3db82eb6dbec02c64da75e4f8e50d6bf
PrinzEugen7 <nsi05162@pooae.com>
2017-02-03 16:22:51 +0900

    Create main.js

---
+++ Javascript/switch/main.js
@@ -0,0 +1,20 @@
+function main(){
+
+    var i = 1;
+	  
+    switch (i)
+    {
+        case 1:
+        alert("iは1です。\n");
+
+        case 2:
+        alert("iは2です。\n");
+    
+        case 3:
+        alert("iは3です。\n");
+    
+        default:
+        alert("iは1～3以外です。");
+    }
+
+}
