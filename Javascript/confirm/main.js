8b69720e94a1a0a4068d36d556bc88c85e5a4500
PrinzEugen7 <nsi05162@pooae.com>
2017-02-03 22:49:16 +0900

    Create main.js

---
+++ Javascript/confirm/main.js
@@ -0,0 +1,17 @@
+function main()
+{
+    var flag = confirm("ここは田舎者なのん？"); 
+	// OKが押されたら
+    if (flag == true)
+    {
+        alert("やっぱり田舎なのんなー");
+    }
+	// キャンセルが押されたら
+    else if (flag == false)
+    {
+        alert("都会なのん！");
+    }
+	else
+	{
+	}
+}
