a2e4c03c57b16998f4eabb0cd2b6749168f77e78
PrinzEugen7 <nsi05162@pooae.com>
2017-02-03 23:06:07 +0900

    Create main.js

---
+++ Javascript/prompt/main.js
@@ -0,0 +1,14 @@
+function main()
+{
+    var message = prompt('にゃんぱす？'); 
+	  // OKが押されたら
+    if (message == 'にゃんぱす')
+    {
+        alert("ありがとなのん！");
+    }
+
+	else
+	{
+	    alert("ノリわるいのんなー");
+	}
+}
