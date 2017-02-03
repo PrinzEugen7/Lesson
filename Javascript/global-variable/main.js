2c64426eaddb6306ed534a974ad0f3b708a6f201
PrinzEugen7 <nsi05162@pooae.com>
2017-02-03 19:09:01 +0900

    Create main.js

---
+++ Javascript/global-variable/main.js
@@ -0,0 +1,21 @@
+// グローバル変数の宣言
+var x = 10;
+var y = 20;
+
+function test()
+{
+    // ローカル変数の宣言
+    var z = 0;
+  
+    z = x + y;
+	
+}
+
+function main()
+{
+    // ローカル変数の宣言
+    var z = 0;
+  
+    z = x - y;
+
+}
