648e7550f8160208e262ed5d7b5b17dc5746f8d5
PrinzEugen7 <nsi05162@pooae.com>
2017-02-03 23:12:25 +0900

    Create main.js

---
+++ Javascript/location-href/main.js
@@ -0,0 +1,9 @@
+function main()
+{
+    var flag = confirm('吹雪「司令官、鎮守府に向かいますか？」'); 
+	  // OKが押されたら
+    if (flag == true)
+    {
+        location.href = "http://www.dmm.com/netgame/feature/kancolle.html";
+    }
+}
