36f158ae67a29f271da91654a92bbf6a954591c1
PrinzEugen7 <nsi05162@pooae.com>
2017-02-03 16:24:20 +0900

    Create main.js

---
+++ Javascript/switdh-break/main.js
@@ -0,0 +1,24 @@
+function main(){
+
+    var i = 1;
+	  
+    switch (i)
+    {
+        case 1:
+        alert("iは1です。\n");
+		break;
+
+        case 2:
+        alert("iは2です。\n");
+		break;
+		
+        case 3:
+        alert("iは3です。\n");
+		break;
+		
+        default:
+        alert("iは1～3以外です。");
+		break;
+    }
+
+}
