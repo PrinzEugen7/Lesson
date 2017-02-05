0e2898563a633aa5e80fbfca86678e294d42cf05
PrinzEugen7 <nsi05162@pooae.com>
2017-02-05 12:56:54 +0900

    Update main.js

--- Javascript/window/open/option/main.js
+++ Javascript/window/open/option/main.js
@@ -3,17 +3,17 @@ function main()
     // URL
     var url = 'http://www.dmm.com/netgame_s/kancolle/';
 	
-	// ウィンドウのサイズ
-	var size = 'width = 800, height = 600'; 
+    // ウィンドウのサイズ
+    var size = 'width = 800, height = 600'; 
 	
-	// ウィンドウの位置
-	var loc = 'left = 0, top = 600'; 
+    // ウィンドウの位置
+    var loc = 'left = 0, top = 600'; 
 	
-	// 各種バーの設定
-	var bar = 'scrollbars = no, toolbar = no, menubar = no, status = no, directories = no';
+    // 各種バーの設定
+    var bar = 'scrollbars = no, toolbar = no, menubar = no, status = no, directories = no';
 	
-	// その他の設定
-	var other = 'resizable = no';
+    // その他の設定
+    var other = 'resizable = no';
 	
     // ウィンドウを表示
     window.open(url, '艦これ' , size + ', ' + loc + ', '  + bar + ', ' + other);
