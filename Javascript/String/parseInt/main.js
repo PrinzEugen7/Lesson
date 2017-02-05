0a983b7771e6de6da942a8cc4bcacc53f64d98a7
PrinzEugen7 <nsi05162@pooae.com>
2017-02-05 13:24:00 +0900

    Create main.js

---
+++ Javascript/String/parseInt/main.js
@@ -0,0 +1,13 @@
+function main()
+{	
+    // 変数の宣言・初期化
+    var x1 = '10';
+    var x2 = '10';
+	
+    // 計算
+    var y1 = x1 + x2; 
+    var y2 = parseInt(x1) + parseInt(x2); 
+		
+    // 結果表示
+    alert('y1 = ' + y1 + '\ny2 = ' + y2 );
+}
