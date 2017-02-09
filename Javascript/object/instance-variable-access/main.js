2f7a1ad81fc2a0057a59170867470d750b5e11d8
PrinzEugen7 <nsi05162@pooae.com>
2017-02-09 19:31:32 +0900

    Create main.js

---
+++ Javascript/object/instance-variable-access/main.js
@@ -0,0 +1,15 @@
+// クラスの定義
+var Kanmusu = function()
+{
+    // メンバ変数(インスタンス変数)
+    this.name;
+    this.type;
+}
+
+function main()
+{
+    // インスタンスの生成
+    var fubuki = new Kanmusu();
+    fubuki.name = "吹雪";
+    fubuki.type = "駆逐艦";
+}
