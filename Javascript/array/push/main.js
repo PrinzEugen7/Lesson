8661ed56316bfce9751d81b5a4fb2ea7bb4064bd
PrinzEugen7 <nsi05162@pooae.com>
2017-02-10 23:31:19 +0900

    Update main.js

--- Javascript/array/push/main.js
+++ Javascript/array/push/main.js
@@ -2,8 +2,8 @@ function main()
 {
     // 配列を宣言・初期化
     data = new Array('沖田', '武蔵', 'モードレッド');
-	  // 処理
-    result = data.push();
-	  // 結果表示
-    alert( result );
+    // 処理
+    result = data.push('アルトリア');
+    // 結果表示
+    alert( "data=" + data + "\n" + "result=" +result );
 }
