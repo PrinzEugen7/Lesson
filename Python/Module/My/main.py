1fd9898c0b5f85ae6ac2d3a3c0e31dfb7b981b60
PrinzEugen7 <nsi05162@pooae.com>
2017-02-20 18:48:07 +0900

    Create main.py

---
+++ Python/Module/My/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import my
+
+def main():
+    # インスタンスの生成
+    my_class = my.MyClass()
+    # メソッドの呼び出し
+    my_class.my_method("にゃんぱすー")
+
+if __name__ == "__main__":
+    main()
