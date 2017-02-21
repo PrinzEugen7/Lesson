642bd9aaaccbed8a7c57d617182d184c8c9172d7
PrinzEugen7 <nsi05162@pooae.com>
2017-02-21 22:19:06 +0900

    Create dataframe_index.py

---
+++ Python/Pandas/dataframe_index.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+def main():
+    # データフレームの初期化
+    df = pd.DataFrame({
+    '名前' : ['西住みほ', '秋山優花里', '武部沙織'],
+    '身長' : [158, 157, 157]},
+    index = ['車長', '装填手', '通信手']
+    )
+    # 表示
+    print(df)
+
+if __name__ == '__main__':
+    main()
