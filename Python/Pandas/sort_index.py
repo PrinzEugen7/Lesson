015b435184cab65da1076f2d5732f2a73dc0f121
PrinzEugen7 <nsi05162@pooae.com>
2017-02-22 00:35:36 +0900

    Create sort_index.py

---
+++ Python/Pandas/sort_index.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+def main():
+    # データフレームの初期化
+    df = pd.DataFrame({
+    '名前' : ['西住みほ', '秋山優花里', '武部沙織'],
+    '身長' : [158, 157, 157]},
+    index = ['車長', '装填手', '通信手']
+    )
+    df2 = df.sort_index(axis=1, ascending=False)
+    # 表示
+    print(df2)
+
+if __name__ == "__main__":
+    main()
