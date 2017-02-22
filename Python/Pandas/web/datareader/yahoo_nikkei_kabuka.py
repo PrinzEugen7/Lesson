75d713306cfd2eafdac562c262e0be0032bbb108
PrinzEugen7 <nsi05162@pooae.com>
2017-02-22 19:33:37 +0900

    Create yahoo_nikkei_kabuka.py

---
+++ Python/Pandas/web/datareader/yahoo_nikkei_kabuka.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import pandas_datareader.data as web
+import datetime
+
+def main():
+    start = datetime.datetime(2017, 2, 10)
+    end = datetime.datetime(2017, 2, 22)
+    f = web.DataReader('^N225', 'yahoo', start, end)
+    print(f)
+
+
+if __name__ == '__main__':
+    main()
