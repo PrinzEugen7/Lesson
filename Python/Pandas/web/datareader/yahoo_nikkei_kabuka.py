20631b0b9116571ed5282a027a2b3777690f0434
PrinzEugen7 <nsi05162@pooae.com>
2017-02-22 19:34:48 +0900

    Update yahoo_nikkei_kabuka.py

--- Python/Pandas/web/datareader/yahoo_nikkei_kabuka.py
+++ Python/Pandas/web/datareader/yahoo_nikkei_kabuka.py
@@ -3,7 +3,7 @@ import pandas_datareader.data as web
 import datetime
 
 def main():
-    start = datetime.datetime(2017, 2, 10)
+    start = datetime.datetime(2017, 2, 20)
     end = datetime.datetime(2017, 2, 22)
     f = web.DataReader('^N225', 'yahoo', start, end)
     print(f)
