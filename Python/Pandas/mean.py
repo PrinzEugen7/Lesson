b54ef0cb6f4560cf851cb6f9919be92979ffd900
PrinzEugen7 <nsi05162@pooae.com>
2017-02-22 15:48:58 +0900

    Create mean.py

---
+++ Python/Pandas/mean.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+def main():
+    # CSVファイルの読み込み
+    df = pd.read_csv("artoria.csv", index_col=0)
+    # 計算
+    mean = df['ATK'].mean()
+    # 表示
+    print(mean)
+
+
+if __name__ == '__main__':
+    main()
