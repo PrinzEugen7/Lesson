311e270ddbd5d2e58408f4590e304b814ebf1ab1
PrinzEugen7 <nsi05162@pooae.com>
2017-02-26 10:49:36 +0900

    Create main.js

---
+++ Javascript/document/form/select-add/main.js
@@ -0,0 +1,36 @@
+function setSelect( idName, elementName, menu )
+{
+    // 入力ボックスの値を取得
+    var id = document.getElementById('select');
+
+    // メニュー項目のセット
+    for ( var i in menu ) 
+    {
+        var element = document.createElement('option');
+        element.setAttribute('value', i);
+        element.innerHTML = menu[i];
+        id.appendChild( element );
+    }
+}
+
+// 初期化
+function init()
+{
+    // 連想配列
+    var menu = {
+        'Natsumi' : 'なつみ',
+        'Renge'   : 'れんげ',
+        'Hotaru'  : 'ほたる',
+        'Komari'  : 'こまり'
+    };
+    // selectの初期化
+    setSelect( 'select', 'option', menu );
+}
+
+function main()
+{
+    // 入力ボックスの値を取得
+    var str = document.formName.selectName.value;
+    // 結果表示
+    alert(str);  
+}
