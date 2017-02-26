8f69e38f72d82ab7358865c75bf98a4fb94dca16
PrinzEugen7 <nsi05162@pooae.com>
2017-02-26 19:55:59 +0900

    Update main.js

--- Javascript/sample/wareki-year/main.js
+++ Javascript/sample/wareki-year/main.js
@@ -2,19 +2,19 @@ function wareki2seireki(nengo, wareki)
 {
     if ((nengo == "平成") && (wareki > 0)) 
     {
-        return wareki + 1988;
+        return parseInt(wareki) + 1988;
     }
     else if ((nengo == "昭和") && (wareki > 0) && (wareki <= 64)) 
     {
-        return wareki + 1925;
+        return parseInt(wareki) + 1925;
     }
     else if ((nengo == "大正") && (wareki > 0) && (wareki <= 15)) 
     {
-        return wareki + 1911;
+        return parseInt(wareki) + 1911;
     }
     else if ((nengo == "明治") && (wareki > 0) && (wareki <= 45))
     {
-        return wareki + 1867;
+        return parseInt(wareki) + 1867;
     }
     else{}
 }
@@ -82,14 +82,30 @@ function init()
 	var seirekiYear = createAssArray(1931, 2017);
     // selectの初期化
     setSelect( 'nengo', 'option', nengo );
-    setSelect( 'nengo-year', 'option', nengoYear );
-    setSelect( 'seireki-year', 'option', seirekiYear );
+    setSelect( 'nengoYear', 'option', nengoYear );
+    setSelect( 'seirekiYear', 'option', seirekiYear );
 }
 
-function main()
+// 西暦→和暦
+function ToWareki()
 {
     // 入力ボックスの値を取得
-    var str = document.formName.selectName.value;
+    var seireki = document.toWareki.seireki.value;
+    wareki = seireki2wareki(seireki);
+	var result = "西暦" + seireki + "年 → " + wareki + "年"
     // 結果表示
-    alert(str);  
+	document.result.box.value = result + "\n" + document.result.box.value;
+
+}
+
+// 和暦→西暦
+function ToSeireki()
+{
+    // 入力ボックスの値を取得
+    var nengo = document.toSeireki.nengo.value;	
+    var nengoYear = document.toSeireki.nengoYear.value;
+    seireki = wareki2seireki(nengo, nengoYear);
+	result = nengo + nengoYear +"年 → 西暦" + seireki + "年"
+    // 結果表示
+	document.result.box.value = result + "\n" + document.result.box.value;
 }
