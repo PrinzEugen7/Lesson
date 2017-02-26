44d812bdc4cd4178ffeb402f4e059928028bf536
PrinzEugen7 <nsi05162@pooae.com>
2017-02-26 10:55:07 +0900

    Create format.js

---
+++ Javascript/document/form/select-add/format.js
@@ -0,0 +1,10 @@
+// selectタグのID取得
+var select = document.getElementById('select');
+// option要素の宣言
+var option = document.createElement('option');
+// option要素のvalue属性に値をセット
+option.setAttribute('value', 値);
+// option要素に値をセット
+option.innerHTML = 値;
+// 作成したoption要素をselectタグに追加
+select.appendChild(option);
