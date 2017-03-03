7411aab2e187f6051dc4582fee9b0a5c7e74583c
PrinzEugen7 <nsi05162@pooae.com>
2017-03-03 16:05:23 +0900

    Create list.py

---
+++ Python/for/in/list.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    # リストの初期化
+    names = ["宮内ひかげ", "富士宮このみ", "加賀山楓 "]
+    # 結果表示
+    for name in names:
+        print(name)
+
+if __name__ == '__main__':
+    main()
