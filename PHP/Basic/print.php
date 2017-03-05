2f318356c62eeb275785326dabe3148da8f6e66b
PrinzEugen7 <nsi05162@pooae.com>
2017-03-05 14:45:21 +0900

    Create print.php

---
+++ PHP/Basic/print.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>サンプル</title>
+</head>
+<body>
+
+<?php
+
+print 'やっぱりここって<br>田舎なのん'
+echo 'やっぱりここって<br>田舎なのん'
+
+?>
+
+</body>
+</html>
