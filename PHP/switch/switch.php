7fc000cb255501d3a0897498d0428ecd26a31d28
PrinzEugen7 <nsi05162@pooae.com>
2017-03-06 18:28:37 +0900

    Create switch.php

---
+++ PHP/switch/switch.php
@@ -0,0 +1,18 @@
+<?php
+
+$i = 1;
+
+switch ($i)
+{
+    case 1:
+    print "iは1です。\n";
+
+    case 2:
+    print "iは2です。\n";
+    
+    case 3:
+    print "iは3です。\n";
+    
+    default:
+    print "iは1～3以外です。\n";
+}
