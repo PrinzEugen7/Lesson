d9298465e812ac672a1436f3aae008af6dbf06a6
PrinzEugen7 <nsi05162@pooae.com>
2017-03-06 18:30:18 +0900

    Create  switch-break.php

---
+++ PHP/switch-break/ switch-break.php
@@ -0,0 +1,23 @@
+<?php
+
+$i = 1;
+
+switch ($i)
+{
+    case 1:
+    print "iは1です。\n";
+    break;
+
+    case 2:
+    print "iは2です。\n";
+    break;
+    
+    case 3:
+    print "iは3です。\n";
+    break;
+    
+    default:
+    print "iは1～3以外です。\n";
+    break;
+
+}
