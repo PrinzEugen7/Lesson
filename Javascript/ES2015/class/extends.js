7b7ac79dcfdee421e3df84b151ffea6eeaf0e7fa
PrinzEugen7 <nsi05162@pooae.com>
2017-03-09 23:02:29 +0900

    Create extends.js

---
+++ Javascript/ES2015/class/extends.js
@@ -0,0 +1,27 @@
+class Fubuki {
+  constructor(name='吹雪', arm1='10cm連装高角砲', arm2='61cm三連装魚雷') {
+    this.name = name;
+    this.arm1 = arm1;
+    this.arm2 = arm2;
+  }
+  alertArm() {
+    alert('装備1:' + this.arm1 + '\n装備2:' + this.arm2);    
+  }
+}
+
+class FubukiKai extends Fubuki {
+  constructor(name='吹雪', arm1='10cm連装高角砲', arm2='61cm三連装魚雷', arm3 = '未装備') {
+    this.name = name;
+    this.arm1 = arm1;
+    this.arm2 = arm2;
+    this.arm3 = arm3;
+  }
+  alertArm() {
+    alert('装備1:' + this.arm1 + '\n装備2:' + this.arm2 + '\n装備3:' + this.arm3);   
+  }
+}
+
+function main() {
+    const fubukiKai = new FubukiKai();
+    fubukiKai.alertArm();
+}
