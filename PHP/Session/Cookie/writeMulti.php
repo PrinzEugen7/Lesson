d4023086dd364b0330de5a0ced7861ba67dd7d10
PrinzEugen7 <nsi05162@pooae.com>
2017-03-10 22:22:30 +0900

    Create writeMulti.php

---
+++ PHP/Session/Cookie/writeMulti.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Test</title>
+
+<?php
+
+setcookie('クッキー名1', 'クッキーの値1');
+setcookie('クッキー名2', 'クッキーの値2');
+
+?>
+</head>
+  
+<body>
+
+</body>
+</html>
