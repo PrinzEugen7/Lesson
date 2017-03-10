2abb99c5d1e94d11e68acde9659db14a1eb96fa5
PrinzEugen7 <nsi05162@pooae.com>
2017-03-10 20:05:27 +0900

    Update login.php

--- PHP/Session/login.php
+++ PHP/Session/login.php
@@ -1,4 +1,4 @@
-<!<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -15,19 +15,18 @@ if (!isset($_SERVER['PHP_AUTH_USER'])){
     // ログイン画面を表示
     header('WWW-Authenticate: Basic realm="Private Page"');
     header('HTTP/1.0 401 Unauthorized');
-	// 「キャンセル」ボタンが押された場合
+　　　　// 「キャンセル」ボタンが押された場合
     die('ページを閲覧するにはログインしてください');
 }else{
     // ログイン画面で「OK」ボタンが押された場合、ユーザ名とパスをチェック
     if ($_SERVER['PHP_AUTH_USER'] != $user || $_SERVER['PHP_AUTH_PW'] != $password){
-
         header('WWW-Authenticate: Basic realm="Private Page"');
         header('HTTP/1.0 401 Unauthorized');
-		// ユーザ名とパスが間違っている場合
+	// ユーザ名とパスが間違っている場合
         die('ユーザ名かパスが間違っています');
     }
-	    // ユーザ名とパスが合っていた場合
-        die(' ログインに成功しました。');
+　　　　// ユーザ名とパスが合っていた場合
+    die(' ログインに成功しました。');
 }
 
 ?>
