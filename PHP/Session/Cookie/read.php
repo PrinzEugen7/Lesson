870e82404f35408e7cd0c80d53afa61d8f08e02b
PrinzEugen7 <nsi05162@pooae.com>
2017-03-10 21:40:43 +0900

    Create read.php

---
+++ PHP/Session/Cookie/read.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Test</title>
+
+<?php
+  $value = $_COOKIE["クッキー名"];
+  print "値：$value <br>";
+	?>
+</head>
+  
+<body>
+
+</body>
+</html>
