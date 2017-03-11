d6f6e675393a342f937f06290293eb5e954a300e
PrinzEugen7 <nsi05162@pooae.com>
2017-03-11 13:39:52 +0900

    Create changeName.php

---
+++ PHP/Session/changeName.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Test</title>
+
+<?php
+
+// セッション開始
+session_start();
+// セッション名の変更
+session_name("fatego");
+// セッション名の取得
+$name = session_name();
+// セッション名の表示
+print "現在のセッション名は：$name";
+
+?>
+</head>
+  
+<body>
+</body>
+</html>
