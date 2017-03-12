0a71c35f4d6902f2167c9e8c053f28bb691c6ac3
PrinzEugen7 <nsi05162@pooae.com>
2017-03-12 23:12:50 +0900

    Create method.php

---
+++ PHP/class/method.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// クラス定義
+class Fubuki{
+  // メンバ変数(インスタンス変数)
+  public $name = "艦名";
+  public $arm1 = "装備1";
+  public $arm2 = "装備2";
+  // コンストラクタ
+  function printName(){
+    print "$this->name";
+  }
+
+}
+
+// インスタンス生成(1番艦吹雪)
+$fubuki = new Fubuki();
+
+// 表示
+$fubuki->printName();
