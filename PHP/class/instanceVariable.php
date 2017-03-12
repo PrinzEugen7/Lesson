ca85291cbec8bd7bb472fb2785539b2e8f304e80
PrinzEugen7 <nsi05162@pooae.com>
2017-03-12 22:20:54 +0900

    Create instanceVariable.php

---
+++ PHP/class/instanceVariable.php
@@ -0,0 +1,15 @@
+    <?php
+     
+    // インスタンス生成
+    $fubuki = new Fubuki();
+    $shirayuki = new Fubuki();
+     
+    // クラス定義
+    class Fubuki{
+      // メンバ変数(インスタンス変数)
+      public $name = "艦名";
+      public $arm1 = "装備1";
+      public $arm2 = "装備2";
+    }
+    
+    ?>
