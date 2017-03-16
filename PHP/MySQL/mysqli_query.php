2194259962554a06858064363284981711792355
PrinzEugen7 <nsi05162@pooae.com>
2017-03-16 18:31:04 +0900

    Create mysqli_query.php

---
+++ PHP/MySQL/mysqli_query.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Test</title>
+
+<?php
+
+// DB接続
+$db = new mysqli('localhost', 'root', 'password', 'sarvant');
+
+if (!$db) {
+    print "DB接続失敗<br>";
+}
+
+print "DB接続成功<br>";
+
+// データベースから値を取得
+$cmd = 'SELECT name FROM artoria';
+$result = mysqli_query($db, $cmd);
+if (!$result) {
+    die('クエリーが失敗しました。'.mysqli_error($db));
+}
+
+// 取得した値を表示 
+while ($row = mysqli_fetch_assoc($result)) {
+    print($row['name'].'<br>');
+}
+
+// DB切断
+$closed_flag = mysqli_close($db);
+
+if (!$closed_flag){
+    print "DB切断失敗<br>";
+}
+
+print "DB切断成功<br>";
+
+?>
+</head>
+  
+<body>
+</body>
+</html>
