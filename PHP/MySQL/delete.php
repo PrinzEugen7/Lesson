efe4d170a029f9f42b2b154ef703e038dffb390d
PrinzEugen7 <nsi05162@pooae.com>
2017-03-16 21:33:31 +0900

    Update delete.php

--- PHP/MySQL/delete.php
+++ PHP/MySQL/delete.php
@@ -22,15 +22,17 @@ if (!$result1) {
     print("データ追加に失敗<br>");
 }
 
-// データ取得
+// データ削除
 $cmd = 'DELETE FROM artoria WHERE name = "nero claudius"';
 $result2 = mysqli_query($db, $cmd);
 if (!$result2) {
     print("データ取得に失敗<br>");
 }
 
-// 取得した値を表示 
-while ($row = mysqli_fetch_assoc($result2)) {
+// データ取得
+$cmd = 'SELECT name FROM artoria';
+$result3 = mysqli_query($db, $cmd);
+while ($row = mysqli_fetch_assoc($result3)) {
     print($row['name'].'<br>');
 }
 
