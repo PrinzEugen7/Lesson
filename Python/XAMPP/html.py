d057affaf86a616b0e71abcb9b53b0d269da2150
PrinzEugen7 <nsi05162@pooae.com>
2017-03-17 16:40:21 +0900

    Create html.py

---
+++ Python/XAMPP/html.py
@@ -0,0 +1,15 @@
+#!D:\Programming\python\WinPython35\python-3.5.2\python.exe
+# -*- coding: utf-8 -*-
+print("Content-Type: text/html\n")
+
+# ページ表示
+print('<!DOCTYPE html>')
+print('<html>')
+print('<head>')
+print('<meta charset="utf-8" />')
+print('<title>TEST</title>')
+print('</head>')
+print('<body>')
+print('<h1>Artoria</h1>')
+print('</body>')
+print('</html>')
