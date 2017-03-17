f224939571aa096dcb24c6125d4a67e8b052aa64
PrinzEugen7 <nsi05162@pooae.com>
2017-03-18 00:54:02 +0900

    Create update.py

---
+++ Python/MySQL/mysql-connector/update.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import mysql.connector
+
+def main():
+    # MySQL接続
+    cnt = mysql.connector.connect(
+        host='localhost',
+        port='3306',
+        db='sarvant',
+        user='root',
+        password='password',
+        charset='utf8'
+    )
+
+    # カーソル取得
+    db = cnt.cursor(buffered=True)
+
+    # SQLクエリ実行（データ取得）
+    sql = 'UPDATE artoria SET name = "nero bride" where name = "nero claudius";';
+    db.execute(sql)
+
+    # 表示
+    rows = db.fetchall()
+    print(rows)
+
+    # カーソル終了
+    db.close()
+    # MySQL切断
+    cnt.close()
+
+if __name__ == "__main__":
+    main()
