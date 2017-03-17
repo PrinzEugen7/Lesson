6f247dc70e35c5b9e0d2a2ecaf52a3bf9ac116d5
PrinzEugen7 <nsi05162@pooae.com>
2017-03-18 01:13:14 +0900

    Update delete.py

--- Python/MySQL/mysql-connector/delete.py
+++ Python/MySQL/mysql-connector/delete.py
@@ -15,10 +15,18 @@ def main():
     # カーソル取得
     db = cnt.cursor(buffered=True)
 
-    # SQLクエリ実行（データ取得）
-    sql = 'DELETE FROM artoria WHERE name = "nero claudius";'
+    # SQLクエリ実行（データ追加）
+    sql = 'INSERT INTO artoria(name) VALUES("nero claudius")'
     db.execute(sql)
 
+    # SQLクエリ実行（データ削除）
+    sql = 'DELETE FROM artoria WHERE name = "nero claudius"'
+    db.execute(sql)
+    
+    # SQLクエリ実行（データ取得）
+    sql = 'SELECT name FROM artoria';
+    db.execute(sql)
+    
     # 表示
     rows = db.fetchall()
     print(rows)
