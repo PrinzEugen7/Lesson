b4e4343a99c0ad9cbb2e1ed80a68734b375155c5
PrinzEugen7 <nsi05162@pooae.com>
2017-03-18 15:25:50 +0900

    Update insert_into.py

--- Python/SQLite/insert_into.py
+++ Python/SQLite/insert_into.py
@@ -1,25 +1,22 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8
 import sqlite3
 
 def main():
-    # DBに接続
-    db = sqlite3.connect("sarvant.db")
+    # データベース開く
+    db = sqlite3.connect('sarvant.db')
+    c = db.cursor()
     # テーブル作成
-    sql = "create table artoria (name varchar, class varchar, atk integer, hp integer, rank varchar);"
-    db.execute(sql)
-    # レコード登録（データ追加）
-    db.execute("insert into artoria values ('artoria','saver','11221','15150','5')")
-    db.execute("insert into artoria values ('artoria alter','saver','10248','11589','4')")
-    db.execute("insert into artoria values ('artoria lily','savar','7726','10623','4')")
-    db.execute("insert into artoria values ('artoria lancer','lancer','10995','15606','5')")
-    db.execute("insert into artoria values ('artoria lancer alter','lancer','9968','11761','4')")
-    db.execute("insert into artoria values ('artoria swimwear','archer','11276','14553','5')")
-    db.execute("insert into artoria values ('artoria santa alter','rider','9258','11286','4')")
-    db.execute("insert into artoria values ('mystery heroine x','assassin','11761','12696,'5')")
-    db.execute("insert into artoria values ('mystery heroine x alter','berserker','11113','14175','5')")
-    # 処理実行
+    c.execute('create table artoria (name text, atk int, hp int)')
+
+    # データ追加(レコード登録)
+    sql = 'insert into artoria (name, atk, hp) values (?,?,?)'
+    data = ('artoria',11221,15150)
+    c.execute(sql, data)
+
+    # コミット
     db.commit()
-    # DB閉じる
+
+    # クローズ
     db.close()
 
 if __name__ == "__main__":
