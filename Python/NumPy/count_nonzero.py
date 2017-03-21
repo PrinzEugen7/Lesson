11b5e19a0d7518c9926948f98e24bea9169da190
PrinzEugen7 <nsi05162@pooae.com>
2017-03-21 11:14:27 +0900

    Update count_nonzero.py

--- Python/NumPy/count_nonzero.py
+++ Python/NumPy/count_nonzero.py
@@ -4,9 +4,9 @@ import numpy as np
     
 def main():
     # 配列生成
-    A = np.array([0, 1, 1, 1, 0, 0])
+    A = np.array([0, 1, 1, 1, 2, 0, 0])
     
-    # 要素が0の個数をカウント
+    # 値が0でない要素数をカウント
     num = np.count_nonzero(A)
     
     # 結果表示
