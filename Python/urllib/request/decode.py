163be981f0fb1e7f1a3ad346aefe7443aeebeded
PrinzEugen7 <nsi05162@pooae.com>
2017-03-21 14:37:40 +0900

    Create decode.py

---
+++ Python/urllib/request/decode.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8
+import urllib
+    
+def main():
+    # 取得先URL
+    url = "https://algorithm.joho.info/" 
+ 
+    # HTMLファイルを開く
+    data = urllib.request.urlopen(url)
+
+    # HTMLの取得      
+    html = data.read()
+    html = html.decode('utf-8')
+    
+    # 表示
+    print(html)
+    
+    # HTMLファイルを閉じる
+    data.close()
+   
+    
+if __name__ == "__main__":
+    main()
