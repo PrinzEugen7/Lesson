f915098ced44da27df8786632e193172a90105b5
PrinzEugen7 <nsi05162@pooae.com>
2017-03-23 00:05:11 +0900

    Create moving_average.py

---
+++ Python/NumPy/Stock/Nikkei/moving_average.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 移動平均線の計算(データ, 日数)
+def move_average(data, day):
+    return np.convolve(data, np.ones(day)/float(day), 'valid')
+    
+def main():
+     # CSVのロード(2015年と2016年のデータ)
+    data15 = np.genfromtxt("nikkei15.csv", delimiter=",", skip_header=1, dtype='float')
+    data16 = np.genfromtxt("nikkei16.csv", delimiter=",", skip_header=1, dtype='float')
+    # 5列目の終値だけを取り出し
+    f15 = data15[:,4]
+    f16 = data16[:,4]
+    
+    # 移動平均線(25日線)の計算
+    day = 25    # 日数
+    data = np.r_[f15[len(f15)-day+1:len(f15)], f16]    #　2015年の終値の一部と2016年の終値を結合
+    ma_25d = move_average(data, day)
+
+    # 移動平均線(75日線)の計算
+    day = 75    # 日数
+    data = np.r_[f15[len(f15)-day+1:len(f15)], f16]    #　2015年の終値の一部と2016年の終値を結合
+    ma_75d = move_average(data, day)
+    
+    # グラフにプロット
+    plt.plot(f16,  label="f")
+    plt.plot(ma_25d, label="MA 25d")
+    plt.plot(ma_75d, label="MA 75d")    
+    
+    #　ラベル軸
+    plt.xlabel("Day")
+    plt.ylabel("f")
+    # 凡例
+    plt.legend()
+    # グリッド
+    plt.grid()
+    # グラフ表示
+    plt.show()
+
+    
+if __name__ == "__main__":
+    main()
