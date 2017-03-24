88dd35c67a0b9501177ab9163a888460d7092851
PrinzEugen7 <nsi05162@pooae.com>
2017-03-24 12:42:36 +0900

    Create log.py

---
+++ Python/NumPy/log.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8
+import numpy as np
+    
+def main():
+    # 配列生成
+    x = np.array([1, 2, 3, 4, 5, 6])
+    
+    # 計算
+    y = np.log(x)
+    
+    # 表示
+    print(y)
+
+if __name__ == "__main__":
+    main()
