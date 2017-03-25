48f267b23d580c8114cdecfd54f284f5302b66e9
PrinzEugen7 <nsi05162@pooae.com>
2017-03-25 22:41:26 +0900

    Create loop.py

---
+++ Python/Tuple/loop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def main():
+	# タプルの生成
+    data = (1, 2, 3, 4, 5)
+    
+    data = data * 2
+    
+    # 表示
+    print(data) # (1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+
+    
+if __name__ == '__main__':
+    main()
