045d9397ddb98b508066f44e80efffbe3794929b
PrinzEugen7 <nsi05162@pooae.com>
2017-03-25 22:40:17 +0900

    Create connect.py

---
+++ Python/Tuple/connect.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def main():
+	# タプルの生成
+    data1 = (1, 2, 3, 4, 5)
+    data2 = (6, 7, 8, 9, 10)
+    
+    data = data1 + data2;
+    # 表示
+    print(data) # (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    
+if __name__ == '__main__':
+    main()
