bec93527e8c28b6bdc844d027e038331aab4c0c0
PrinzEugen7 <nsi05162@pooae.com>
2017-03-25 22:44:11 +0900

    Create list_to_tuple.py

---
+++ Python/Tuple/list_to_tuple.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def main():
+	# リストの生成
+    data = [1, 2, 3, 4, 5]
+    
+    # リストの表示
+    print(data) # [1, 2, 3, 4, 5]
+    
+    # タプルに変換
+    data = tuple(data)
+    
+    # タプルの表示
+    print(data) # (1, 2, 3, 4, 5)
+
+    
+if __name__ == '__main__':
+    main()
