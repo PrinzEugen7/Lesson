1eb8f325081ad34bd3e036fc48c09fe8b748dcdb
PrinzEugen7 <nsi05162@pooae.com>
2017-03-26 19:05:07 +0900

    Create ie_open_url.py

---
+++ Python/Network/webbrowser/ie_open_url.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import webbrowser
+
+def main():
+    url = "http://www.nicovideo.jp/"
+    browser = webbrowser.get('"c:\\program files\\internet explorer\\iexplore.exe" %s')
+    browser.open(url)
+
+if __name__ == "__main__":
+    main()
