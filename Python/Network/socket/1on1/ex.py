8b70e8e61659358004e4501dac3e1c6655a15c30
PrinzEugen7 <nsi05162@pooae.com>
2017-03-26 15:56:32 +0900

    Create ex.py

---
+++ Python/Network/socket/1on1/ex.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import socket
+
+def main():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.connect(("localhost", 50007))
+
+    while(1):
+        data = soc.recv(1024)
+        print "Server>", data       # サーバー側の書き込みを表示
+        data = raw_input("Client>") # 入力待機
+        soc.send(data)              # ソケットに入力したデータを送信
+
+        if data == "q":             # qが押されたら終了
+            soc.close()
+            break
+
+if __name__ == '__main__':
+    main()
