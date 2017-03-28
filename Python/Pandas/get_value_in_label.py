4955ab5871090f89dcb6181e0d47c72534b7d5a8
PrinzEugen7 <nsi05162@pooae.com>
2017-03-28 19:07:14 +0900

    Create get_value_in_label.py

---
+++ Python/Pandas/get_value_in_label.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+import pandas as pd
+
+    
+def main():
+    # リスト生成
+    name = ['西住みほ', '秋山優花里', '武部沙織']
+    height = [158, 157, 157]
+    position = ['車長', '装填手', '通信手']
+    
+    # リスト→データフレーム
+    df = pd.DataFrame({'名前' : name, '身長' : height}, index = position)
+
+    # データフレーム表示
+    print(df)
+    
+    '''
+        　　　　名前   身長
+    車長    西住みほ  158
+    装填手  秋山優花里  157
+    通信手   武部沙織  157
+    '''
+
+    # ラベル「名前」の値を表示
+    print(df['名前'].values) # ['西住みほ' '秋山優花里' '武部沙織']
+
+
+if __name__ == "__main__":
+    main()
+
