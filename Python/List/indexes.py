5c0aa8200f5862fe44bfc4e5139523398832acf2
PrinzEugen7 <nsi05162@pooae.com>
2017-03-29 21:50:52 +0900

    Update indexes.py

--- Python/List/indexes.py
+++ Python/List/indexes.py
@@ -1,8 +1,11 @@
 #-*- coding:utf-8 -*-
 
 def main():
-    list = [1,2,3,4,5,4,3,2,1]
-    indexes = [i for i, e in enumerate(list) if e == 3]
+    # リスト生成
+    data = [1,2,3,4,5,4,3,2,1]
+    # 値3をもつ全てのインデックス
+    indexes = [i for i, e in enumerate(data) if e == 3]
+    # 結果表示
     print(indexes) # [2, 6]
     
 if __name__ == "__main__":
