f78a1b6eeb448daca420ac91bfcbf5519a8c0635
PrinzEugen7 <nsi05162@pooae.com>
2017-03-30 23:14:16 +0900

    Create Value.cs

---
+++ CSharp/Variable/Value.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Sample
+{
+    class Test
+    {
+        static void Main() 
+        {
+            // 変数の宣言
+            int    var1 = 1;
+            double var2 = 1.2345;                
+        }
+    }
+}
