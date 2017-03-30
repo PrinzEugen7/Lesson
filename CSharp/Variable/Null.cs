84b57d61216d0923e901c2ede874274e0de0618a
PrinzEugen7 <nsi05162@pooae.com>
2017-03-30 22:11:17 +0900

    Create Null.cs

---
+++ CSharp/Variable/Null.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Sample
+{
+    class Test
+    {
+        static void Main() 
+        {
+            // 変数の宣言
+            string  var1 = null;
+            int?    var2 = null;
+            double? var3 = null;
+            // 変数の中身表示
+            System.Console.WriteLine(var1); // 
+            System.Console.WriteLine(var2); //
+            System.Console.WriteLine(var3); // 
+        }
+    }
+}
