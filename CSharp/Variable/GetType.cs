03a76d7bc1326954e1e17caa0730431f68a3c98f
PrinzEugen7 <nsi05162@pooae.com>
2017-03-30 21:59:49 +0900

    Create GetType.cs

---
+++ CSharp/Variable/GetType.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Sample
+{
+    class Test
+    {
+        static void Main() 
+        {
+            // 変数の宣言
+            string var = "たーのし！";
+		
+            // 変数の中身表示
+            System.Console.WriteLine(var.GetType()); // System.String
+        }
+    }
+}
