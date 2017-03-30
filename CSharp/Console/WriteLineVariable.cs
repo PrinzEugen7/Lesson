88e4e34ce1a2847b7651e953496f262cb159156d
PrinzEugen7 <nsi05162@pooae.com>
2017-03-30 19:14:39 +0900

    Create WriteLineVariable.cs

---
+++ CSharp/Console/WriteLineVariable.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Sample
+{
+    class Test
+    {
+        static void Main() 
+        {
+            // 変数の宣言
+		        int    var1 = 100;
+		        double var2 = 0.01;
+		        float  var3 = 0.01f;
+		        string var4 = "たーのし！";
+		        char   var5 = 'a';
+		
+            // 変数の中身表示
+		        System.Console.WriteLine(var1); //100
+		        System.Console.WriteLine(var2); //0.01
+		        System.Console.WriteLine(var3); //0.01
+		        System.Console.WriteLine(var4); //たーのし！	
+            System.Console.WriteLine(var5); //a
+        }
+    }
+}
