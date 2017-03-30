ce47fc329f6b1038f1316ae16b442176b1574066
PrinzEugen7 <nsi05162@pooae.com>
2017-03-30 19:33:22 +0900

    Update WriteLineVariable.cs

--- CSharp/Console/WriteLineVariable.cs
+++ CSharp/Console/WriteLineVariable.cs
@@ -7,18 +7,18 @@ namespace Sample
         static void Main() 
         {
             // 変数の宣言
-		        int    var1 = 100;
-		        double var2 = 0.01;
-		        float  var3 = 0.01f;
-		        string var4 = "たーのし！";
-		        char   var5 = 'a';
+            int    var1 = 100;
+            double var2 = 0.01;
+            float  var3 = 0.01f;
+            string var4 = "たーのし！";
+            char   var5 = 'a';
 		
             // 変数の中身表示
-		        System.Console.WriteLine(var1); //100
-		        System.Console.WriteLine(var2); //0.01
-		        System.Console.WriteLine(var3); //0.01
-		        System.Console.WriteLine(var4); //たーのし！	
-            System.Console.WriteLine(var5); //a
+            System.Console.WriteLine(var1); // 100
+	    System.Console.WriteLine(var2); // 0.01
+            System.Console.WriteLine(var3); // 0.01
+            System.Console.WriteLine(var4); // たーのし！	
+            System.Console.WriteLine(var5); // a
         }
     }
 }
