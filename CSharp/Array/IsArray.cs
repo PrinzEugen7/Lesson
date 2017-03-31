354932f69a7d3427bcbbdc03f8b06dc7efa642e8
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 15:56:47 +0900

    Update IsArray.cs

--- CSharp/Array/IsArray.cs
+++ CSharp/Array/IsArray.cs
@@ -8,7 +8,7 @@ public class Test
 		int[] score = new int[] {100, 97, 78};
 		
 		// 変数の型をチェック
-	    Type type = score.GetType();
+	        Type type = score.GetType();
 		
 		// 配列ならTrue, 違えばFalse
 		System.Console.WriteLine(type.IsArray); // True
