2f5e847ebd14bf6e8dfff25cf9c73a3ad46043b9
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 19:09:40 +0900

    Create Var.cs

---
+++ CSharp/Array/Var.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public class Test
+{
+	public static void Main()
+	{
+		// 変数宣言
+        var array = new [ ]{1, 2, 3, 4, 5};
+        
+        // 型の確認
+        Console.WriteLine(array.GetType()); // System.Int32[]
+	}
+}
