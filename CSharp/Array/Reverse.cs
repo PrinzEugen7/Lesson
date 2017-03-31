bfb60cecacf0768a6c6001de0200bf52350e5499
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 16:56:50 +0900

    Update Reverse.cs

--- CSharp/Array/Reverse.cs
+++ CSharp/Array/Reverse.cs
@@ -5,15 +5,15 @@ public class Test
 {
 	public static void Main()
 	{
-		// 配列の宣言
-        string[] datas = {"沖田", "武蔵", "モードレッド"};
+            // 配列の宣言
+            string[] datas = {"沖田", "武蔵", "モードレッド"};
         
-        // 配列の要素順序を反転
-        Array.Reverse(datas);
+            // 配列の要素順序を反転
+            Array.Reverse(datas);
         
-		// 配列の中身を表示
-		System.Console.WriteLine(datas[0]); // モードレッド
-		System.Console.WriteLine(datas[1]); // 武蔵
-		System.Console.WriteLine(datas[2]); // 沖田
+	    // 配列の中身を表示
+	    System.Console.WriteLine(datas[0]); // モードレッド
+	    System.Console.WriteLine(datas[1]); // 武蔵
+	    System.Console.WriteLine(datas[2]); // 沖田
 	}
 }
