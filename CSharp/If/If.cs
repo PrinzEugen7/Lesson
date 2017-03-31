65bffd3010933f4cdd41f6589cb570c40c06c28d
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 20:25:20 +0900

    Create If.cs

---
+++ CSharp/If/If.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Test
+{
+    public static void Main()
+    {
+    	// 変数宣言
+        var x = 10;
+        
+        // xが10なら
+        if(x==10){
+            Console.WriteLine("xは10です"); // xは10です
+        }
+    }
+}
