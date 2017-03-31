3832b6bd66c1fdc13e587581aff644db711bd0bb
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 18:52:25 +0900

    Create Var.cs

---
+++ CSharp/Variable/Var.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+
+public class Test
+{
+	public static void Main()
+	{
+		// 変数宣言
+        var a1 = 7;
+        var a2 = "にゃんぱすー";
+        
+        // 型の確認
+        Console.WriteLine(a1.GetType()); // System.Int32
+        Console.WriteLine(a2.GetType()); // System.String
+	}
+}
