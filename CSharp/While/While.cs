fd08cb80b87ead1fa09e3ad4191b02716007b576
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 21:03:22 +0900

    Create While.cs

---
+++ CSharp/While/While.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Test
+{
+    public static void Main()
+    {
+        var i = 1;
+          
+        while(i <= 5){
+            Console.WriteLine(i); // 1, 2, 3, 4, 5
+            i++;
+        }
+
+    }
+}
+
