cdc4bff3f7d3489d3e42de057923a5c452f0210c
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 14:28:03 +0900

    Update Length.cs

--- CSharp/Array/Length.cs
+++ CSharp/Array/Length.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 public class Test
 {
-	public static void Main()
-	{
-      // 配列の生成
-      int[] score = new int[] {100, 97, 78};
+    public static void Main()
+    {
+        // 配列の生成
+        int[] score = new int[] {100, 97, 78};
  
-      // 結果を表示
-      System.Console.WriteLine( score.Length ); // 3
+        // 結果を表示
+        System.Console.WriteLine( score.Length ); // 3
 
-	}
+    }
 }
