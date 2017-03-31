5de7a80e6e286107af0eb7a5d28c228440d21c95
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 20:56:58 +0900

    Create For.cs

---
+++ CSharp/For/For.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class Test
+{
+    public static void Main()
+    {
+        var i = 1;
+          
+        for(i=1; i<=5; i++){ 
+            Console.WriteLine(i); // 1, 2, 3, 4, 5
+        }
+
+    }
+}
