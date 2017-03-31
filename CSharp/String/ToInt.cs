68d0986a344fcdf8e4f7591e6094d58080fa62b1
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 23:43:51 +0900

    Create ToInt.cs

---
+++ CSharp/String/ToInt.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+	{
+		var str = "100";
+
+        int num = int.Parse(str);
+        //double num = double.Parse(str);
+        
+        Console.WriteLine(num); // 100
+
+	}
+}
