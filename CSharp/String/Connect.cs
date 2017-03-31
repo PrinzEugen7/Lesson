17494c26e87ea993a1946bff5fa7fca5257aefe8
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 23:17:35 +0900

    Update Connect.cs

--- CSharp/String/Connect.cs
+++ CSharp/String/Connect.cs
@@ -2,14 +2,14 @@ using System;
 
 public class Test
 {
-	public static void Main()
-	{
-	    var str1 = "にゃん";
-      var str2 = "ぱすー";
+    public static void Main()
+    {
+        var str1 = "にゃん";
+        var str2 = "ぱすー";
         
-      var str = str1 + str2;
+        var str = str1 + str2;
         
-      Console.WriteLine(str); // にゃんぱすー
+        Console.WriteLine(str); // にゃんぱすー
 
-	}
+    }
 }
