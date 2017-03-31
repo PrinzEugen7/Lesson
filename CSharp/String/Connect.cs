8baef9e27800da9959c8ea2b414280767c129feb
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 23:16:41 +0900

    Create Connect.cs

---
+++ CSharp/String/Connect.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+	{
+	    var str1 = "にゃん";
+      var str2 = "ぱすー";
+        
+      var str = str1 + str2;
+        
+      Console.WriteLine(str); // にゃんぱすー
+
+	}
+}
