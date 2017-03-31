c68edcf106282f9b60a6096066f3068749275ef2
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 14:25:35 +0900

    Create Length.cs

---
+++ CSharp/Array/Length.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class Test
+{
+	public static void Main()
+	{
+      // 配列の生成
+      int[] score = new int[] {100, 97, 78};
+ 
+      // 結果を表示
+      System.Console.WriteLine( score.Length ); // 3
+
+	}
+}
