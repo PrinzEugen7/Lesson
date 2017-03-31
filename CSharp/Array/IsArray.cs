d3551e930b9f482bd576512fb8a698c08dda3277
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 15:55:09 +0900

    Create IsArray.cs

---
+++ CSharp/Array/IsArray.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+	{
+		// 配列の宣言
+		int[] score = new int[] {100, 97, 78};
+		
+		// 変数の型をチェック
+	    Type type = score.GetType();
+		
+		// 配列ならTrue, 違えばFalse
+		System.Console.WriteLine(type.IsArray); // True
+	}
+}
