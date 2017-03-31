ce9499b955dddf68d3c76556d9801ac200c5810f
PrinzEugen7 <nsi05162@pooae.com>
2017-03-31 23:47:43 +0900

    Create ToString.cs

---
+++ CSharp/String/ToString.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+	{
+		int num = 100;
+
+        string str = num.ToString();;
+        
+        Console.WriteLine(str); // 100
+
+	}
+}
