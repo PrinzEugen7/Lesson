a2e32244d245b598ea425351429af26d9ec652cb
PrinzEugen7 <nsi05162@pooae.com>
2017-04-01 17:51:35 +0900

    Create InstanceVariable.cs

---
+++ CSharp/Class/InstanceVariable.cs
@@ -0,0 +1,19 @@
+using System;
+
+// クラス定義
+public class TypeFubuki
+{
+    // インスタンス変数
+    public string name;
+}
+
+public class Test
+{
+        public static void Main()
+        {
+                // インスタンス生成
+                TypeFubuki fubuki = new TypeFubuki();
+                fubuki.name = "吹雪";
+
+        }
+}
