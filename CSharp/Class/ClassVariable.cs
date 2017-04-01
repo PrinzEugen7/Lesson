f6e815bc34b1b45003fd3e5a0907da464a6b1276
PrinzEugen7 <nsi05162@pooae.com>
2017-04-01 17:54:48 +0900

    Create ClassVariable.cs

---
+++ CSharp/Class/ClassVariable.cs
@@ -0,0 +1,18 @@
+using System;
+
+// クラス定義
+public class TypeFubuki
+{
+    static string name ="吹雪";
+}
+
+public class Test
+{
+        public static void Main()
+        {
+                // インスタンス生成
+                TypeFubuki fubuki = new TypeFubuki();
+
+        }
+}
+
