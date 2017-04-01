92584d444ba69d0c879ce891de95f5c657960007
PrinzEugen7 <nsi05162@pooae.com>
2017-04-01 20:54:39 +0900

    Create Method.cs

---
+++ CSharp/Class/Method.cs
@@ -0,0 +1,36 @@
+using System;
+
+// クラス定義
+public class TypeFubuki
+{
+	// インスタンス変数
+    public string name ="艦名";
+    public string type ="艦種";
+    
+    // コンストラクタ(初期化メソッド)
+    public TypeFubuki(string srcName, string srcType)
+    {
+        name = srcName;
+        type = srcType;
+    }
+    
+    // メソッド定義
+    public void Print()
+    {
+        // 表示
+        System.Console.WriteLine(type + ":" + name);
+    }
+}
+
+public class Test
+{
+        public static void Main()
+        {
+                // インスタンス生成
+                TypeFubuki fubuki = new TypeFubuki("吹雪", "駆逐艦");
+                
+                // メソッド呼び出し
+                fubuki.Print(); // 駆逐艦:吹雪
+
+        }
+}
