9750547a5b12c56500eec6e155c9568e340c3bdc
PrinzEugen7 <nsi05162@pooae.com>
2017-04-01 17:38:58 +0900

    Create Instance.cs

---
+++ CSharp/Class/Instance.cs
@@ -0,0 +1,17 @@
+using System;
+
+// クラス定義
+public class TypeFubuki
+{
+
+}
+
+public class Test
+{
+        public static void Main()
+        {
+                // インスタンス生成
+                TypeFubuki fubuki = new TypeFubuki();
+
+        }
+}
