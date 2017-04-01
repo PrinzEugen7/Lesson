9347985215910bdb7787050eb36453ba9b4688f3
PrinzEugen7 <nsi05162@pooae.com>
2017-04-01 23:02:11 +0900

    Create Println.cs

---
+++ Java/Println.cs
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+	public static void main (String[] args)
+	{
+		System.out.println("六道五輪・倶利伽羅天象"); // 六道五輪・倶利伽羅天象
+	}
+}
