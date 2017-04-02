c53ebb9aea58ce25e2b97e35ce69bf3dccf931b6
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 16:54:30 +0900

    Update sample.java

--- Java/HashMap/sample.java
+++ Java/HashMap/sample.java
@@ -4,16 +4,16 @@ import java.io.*;
 
 class Test
 {
-        public static void main (String[] args)
-        {       
-        	// オブジェクト生成
-        	HashMap<String,Integer> data = new HashMap<String,Integer>();
+    public static void main (String[] args)
+    {       
+        // オブジェクト生成
+        HashMap<String,Integer> data = new HashMap<String,Integer>();
             
-            // キーと値をセット 
-            data.put("吹雪", 1);
-            data.put("白雪", 2);
+        // キーと値をセット 
+        data.put("吹雪", 1);
+        data.put("白雪", 2);
             
-            // キーから値を取り出し
-            System.out.println(data.get("吹雪")); // 1
-        }
+        // キーから値を取り出し
+        System.out.println(data.get("吹雪")); // 1
+    }
 }
