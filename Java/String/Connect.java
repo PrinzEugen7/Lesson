a9bd9d5bcbce1c618e1e1c25ddc0fc048892d1cb
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 11:53:26 +0900

    Create Connect.java

---
+++ Java/String/Connect.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	    // 変数宣言
+        	    String str1 ="六道五輪";
+        	    String str2 ="・倶利伽羅天象";
+        	    
+        	    // 結果表示
+                System.out.println(str1 + str2); // 六道五輪・倶利伽羅天象
+        }
+}
