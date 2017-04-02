176b656008a2050bb43a9b0882fab5cafcc51834
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 18:29:03 +0900

    Create IsArray.java

---
+++ Java/Array/IsArray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	// 配列の宣言・初期化
+        	String[] name = {"吹雪", "白雪", "深雪"};
+
+            // コンソール出力
+            System.out.println(name.getClass().isArray()); // true
+        }
+}
