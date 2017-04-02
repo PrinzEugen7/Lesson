88f98e67334a7326c099dbdd484a2c96c727c34f
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 11:54:48 +0900

    Create Length.java

---
+++ Java/String/Length.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	    // 変数宣言
+        	    String str ="六道五輪・倶利伽羅天象";
+        	    
+        	    // コンソール出力
+                System.out.println(str.length()); // 11
+        }
+}
