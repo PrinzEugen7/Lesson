dcb5b4738971317e1fd092555484250967cfc3a4
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 19:53:54 +0900

    Create Sort.java

---
+++ Java/Array/Sort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	// 配列の宣言・初期化
+        	int[] score = {100, 51, 72};
+            
+            
+            Arrays.sort(score);
+            
+            // コンソール出力
+            for(int a : score) System.out.println(a); // 深雪, 白雪, 吹雪 
+        }
+}
