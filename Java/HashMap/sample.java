3585b3ff841db95b82e42a1eaf123ae5716d9dd4
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 16:50:20 +0900

    Create sample.java

---
+++ Java/HashMap/sample.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	// オブジェクト生成
+        	HashMap<String,Integer> data = new HashMap<String,Integer>();
+            
+            // キーと値をセット 
+            data.put("吹雪", 1);
+            data.put("白雪", 2);
+            
+            // キーから値を取り出し
+            System.out.println(data.get("吹雪")); // 1
+        }
+}
