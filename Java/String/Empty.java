e1d6fb9f05a6fcc74a5e05ed28bafda20503bcc6
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 11:05:04 +0900

    Create Empty.java

---
+++ Java/String/Empty.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {
+        	    String str = "";
+                System.out.println(str); // 
+        }
+}
+
