4d328dbd66f3625ffb2c28e933d6c2035f69a297
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 18:51:56 +0900

    Update Reverse.java

--- Java/Array/Reverse.java
+++ Java/Array/Reverse.java
@@ -11,12 +11,12 @@ class Test
         	// 配列の宣言・初期化
         	String[] name = {"吹雪", "白雪", "深雪"};
             
-            // 逆順に並び替え
-            List list = Arrays.asList(name);
-            Collections.reverse(list);
-            String[] name2 = (String[])list.toArray(new String[3]);
+              // 逆順に並び替え
+              List list = Arrays.asList(name);
+              Collections.reverse(list);
+              String[] name2 = (String[])list.toArray(new String[3]);
             
-            // コンソール出力
-            for(String n : name2) System.out.println(n); // 深雪, 白雪, 吹雪 
+              // コンソール出力
+              for(String n : name2) System.out.println(n); // 深雪, 白雪, 吹雪 
         }
 }
