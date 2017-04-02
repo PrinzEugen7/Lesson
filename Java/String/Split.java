2187556e1b92b05bd346edeefa2378f2db437df0
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 13:08:43 +0900

    Create Split.java

---
+++ Java/String/Split.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	    // 変数宣言
+        	    String str ="沖田,武蔵,モードレッド";
+        	    
+        	    String[] array = str.split(",");
+        	    
+        	    // コンソール出力
+                System.out.println(array[0]); // 沖田
+                System.out.println(array[1]); // 武蔵
+                System.out.println(array[2]); // モードレッド
+        }
+}
