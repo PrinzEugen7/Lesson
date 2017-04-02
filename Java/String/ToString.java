5d74a767786a8604dcc562f128737807ddba9513
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 13:28:09 +0900

    Create ToString.java

---
+++ Java/String/ToString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	    // 変数宣言
+        	    int num =765;
+        	    
+        	    String str = String.valueOf(num);
+        	    
+        	    // コンソール出力
+                System.out.println(str); // 765
+        }
+}
