d65a856eded1593bfeb8e7f5318ee52351a46aed
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 14:21:05 +0900

    Create SubString.java

---
+++ Java/String/SubString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	    // 変数宣言
+        	    String str = "沖田武蔵モードレッド";
+        	    
+        	    String s = str.substring(2, 4);
+        	    
+        	    // コンソール出力
+                System.out.println(s); // 武蔵
+        }
+}
