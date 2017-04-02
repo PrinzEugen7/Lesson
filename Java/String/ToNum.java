308563c1d7fc92f91b1b6b9bfc1bf79039aed4a3
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 13:11:19 +0900

    Create ToNum.java

---
+++ Java/String/ToNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+        public static void main (String[] args)
+        {       
+        	    // 変数宣言
+        	    String str ="765";
+        	    
+        	    int num = Integer.parseInt(str);
+        	    
+        	    // コンソール出力
+                System.out.println(num); // 765
+        }
+}
