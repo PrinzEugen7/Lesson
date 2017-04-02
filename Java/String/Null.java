845353957ab37abe1f70c960299d57839a6763c0
PrinzEugen7 <nsi05162@pooae.com>
2017-04-02 11:04:14 +0900

    Create Null.java

---
+++ Java/String/Null.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Test
+{
+        public static void main (String[] args)
+        {
+        	    String str = null;
+                System.out.println(str); // 六道五輪・倶利伽羅天象
+        }
+}
+ 
