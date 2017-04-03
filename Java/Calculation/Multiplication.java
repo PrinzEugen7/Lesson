3759953e0d30f122607f2990f7c776d90dd0bc3a
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 17:10:33 +0900

    Create Multiplication.java

---
+++ Java/Calculation/Multiplication.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// 変数宣言
+		int x1 = 10;
+		int x2 = 20;
+		
+		int y = x1 * x2;
+		
+		System.out.println(y); // 200
+	}
+}
