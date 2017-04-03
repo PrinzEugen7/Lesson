8882504095199490d1d382d80e735a3e99f571d4
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 17:57:22 +0900

    Create Definition.java

---
+++ Java/Class/Definition.java
@@ -0,0 +1,18 @@
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+     
+    // クラス宣言
+    class Fubuki
+    {
+     
+    }
+    
+    // メイン
+    class Main
+    {
+    	public static void main (String[] args) throws java.lang.Exception
+    	{
+     
+    	}
+    }
