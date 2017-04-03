5c3d03974c31aba10ffd7991f19902ce4b939acc
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 15:21:34 +0900

    Create While.java

---
+++ Java/While/While.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        // 変数宣言
+        int i = 1;
+        
+        while(i <= 5){
+            System.out.println(i); // 1, 2, 3, 4, 5
+            i++;
+        }
+          
+    }
+}
