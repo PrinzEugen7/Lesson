0b32a40871131b184d5495cc0a84780656d574c5
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 19:11:13 +0900

    Create InstanceVariable.java

---
+++ Java/Class/InstanceVariable.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// メイン
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// インスタンス生成
+        TypeFubuki fubuki = new TypeFubuki();
+	}
+}
+
+// クラス宣言
+class TypeFubuki
+{
+    // インスタンス変数
+    public String name = "吹雪";
+ 
+}
