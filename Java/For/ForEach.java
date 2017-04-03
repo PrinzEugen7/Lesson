c7b559d71e81a04ad02fb2a3f48433a42c767d16
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 15:25:50 +0900

    Create ForEach.java

---
+++ Java/For/ForEach.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        // 変数宣言
+        int score[] = {100, 51, 72};
+   
+        // コンソール出力
+        for(int a : score) System.out.println(a); // 100, 72, 51
+    }
+}
