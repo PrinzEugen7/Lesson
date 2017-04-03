d9e871619ccd998aa12d809584cd5a469a32c714
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 15:22:48 +0900

    Create DoWhile.java

---
+++ Java/While/DoWhile.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        // 変数宣言
+        int i = 1;
+        
+        do{
+            System.out.println(i); // 1, 2, 3, 4, 5
+            i++;    // i = i + 1;と同じ
+        } while(i <= 5);
+
+          
+    }
+}
