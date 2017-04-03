4b93e9e124d6d5f653e1bd1e7ecbf6c4487bce50
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 15:20:07 +0900

    Create For.java

---
+++ Java/For/For.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        // 変数宣言
+        int i = 1;
+          
+        for(i=1; i<=5; i++){ 
+            System.out.println(i); // 1, 2, 3, 4, 5
+        }
+
+    }
+}
