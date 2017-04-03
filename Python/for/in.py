d1eab3088f7ddf1845194a8a04537f49af104ab9
PrinzEugen7 <nsi05162@pooae.com>
2017-04-04 00:21:36 +0900

    Create in.py

---
+++ Python/for/in.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+import sys
+
+def main():
+    # リスト宣言
+    array = ["沖田", "武蔵", "モードレッド"]
+    
+    # インデックスと要素を取り出し
+    for e in array:
+        print(e)
+    
+if __name__ == "__main__":
+    main()
