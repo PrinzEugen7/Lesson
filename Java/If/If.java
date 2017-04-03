d7cd64ea6e84436c21b49263071161d26b1c3eef
PrinzEugen7 <nsi05162@pooae.com>
2017-04-03 15:02:55 +0900

    Create If.java

---
+++ Java/If/If.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        // 変数宣言
+        int x = 10;
+        
+        // xが10なら
+        if(x==10){
+            System.out.println("xは10です"); // xは10です
+        }
+
+    }
+}
+     
