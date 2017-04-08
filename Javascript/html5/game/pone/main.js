7ee7ddec7e28b195fffd6b21cceec46d312abc87
PrinzEugen7 <nsi05162@pooae.com>
2017-04-08 22:06:57 +0900

    Update main.js

--- Javascript/html5/game/pone/main.js
+++ Javascript/html5/game/pone/main.js
@@ -1,16 +1,4 @@
-var cvs;
-var ctx;
-var bx = 50;
-var by = 50;
-var vx = 10;
-var vy = 4;
-var score1p = 0;
-var score2p = 0;
-const WINNING_SCORE = 3;
-var showingWinScreen = false;
 var p1y = 250;
-var p2y = 250;
-
 
 class Pone {
     constructor(name = '吹雪', arm1 = '10cm連装高角砲') {
@@ -18,132 +6,144 @@ class Pone {
         this.mouseY = 0;
         this.paddleW = 10;
         this.paddleH = 100;
+        this.vx = 10;
+        this.vy = 4;
+        this.cvs = null;
+        this.ctx = null;
+        this.score1p = 0;
+        this.score2p = 0;
+        this.scoreMax = 2;
+        this.gameSet = false;
+        this.bx = 50;
+        this.by = 50;
+        this.p2y = 250;
     }
     move() {
-        if (showingWinScreen) {
+        if (this.gameSet) {
             return;
         }
 
-        Pone.cpu();
+        this.cpu();
 
-        bx += vx;
-        by += vy;
+        this.bx += this.vx;
+        this.by += this.vy;
 
-        if (bx < 0) {
-            if (by > p1y && by < p1y + this.paddleH) {
-                vx = -vx;
+        if (this.bx < 0) {
+            if (this.by > p1y && this.by < p1y + this.paddleH) {
+                this.vx = -this.vx;
 
-                var dy = by - (p1y + this.paddleH / 2);
-                vy = dy * 0.35;
+                var dy = this.by - (p1y + this.paddleH / 2);
+                this.vy = dy * 0.35;
             } else {
-                score2p++; // Must come first for win checking
-                Pone.resetBall();
+                this.score2p++;
+                this.resetBall();
 
             }
         }
 
-        if (bx > cvs.width) {
-            if (by > p2y && by < p2y + this.paddleH) {
-                vx = -vx;
+        if (this.bx > this.cvs.width) {
+            if (this.by > this.p2y && this.by < this.p2y + this.paddleH) {
+                this.vx = -this.vx;
 
-                var dy = by - (p2y + this.paddleH / 2);
-                vy = dy * 0.35;
+                var dy = this.by - (this.p2y + this.paddleH / 2);
+                this.vy = dy * 0.35;
             } else {
-                score1p++; // Must come first for win checking
-                Pone.resetBall();
+                this.score1p++;
+                this.resetBall();
 
             }
         }
 
-        if (by > cvs.height || by < 0) {
-            vy = -vy;
+        if (this.by > this.cvs.height || this.by < 0) {
+            this.vy = -this.vy;
         }
     }
 
     draw() {
-        Pone.paddle(0, 0, cvs.width, cvs.height, 'black');
+        this.paddle(0, 0, this.cvs.width, this.cvs.height, 'black');
 
-        if (showingWinScreen) {
-            if (score1p >= WINNING_SCORE) {
-                ctx.fillStyle = 'white';
-                ctx.fillText("Left Player Won!!!", 350, 200);
+        if (this.gameSet) {
+            if (this.score1p >= this.scoreMax) {
+                this.ctx.fillStyle = 'white';
+                this.ctx.fillText("Left Player Won!!!", 350, 200);
             } else {
-                ctx.fillStyle = 'white';
-                ctx.fillText("Right Player Won!!!", 350, 200);
+                this.ctx.fillStyle = 'white';
+                this.ctx.fillText("Right Player Won!!!", 350, 200);
             }
 
-            ctx.fillText("click to continue", 350, 500);
+            this.ctx.fillText("click to continue", 350, 500);
             return;
         }
 
-        Pone.paddle(0, p1y, this.paddleW, this.paddleH, 'white');
-        Pone.paddle(cvs.width - this.paddleW, p2y, this.paddleW, this.paddleH, 'white');
-        Pone.ball(bx, by, 10, 'white');
-        ctx.fillText(score1p, 100, 100);
-        ctx.fillText(score2p, cvs.width - 100, 100);
+        this.paddle(0, p1y, this.paddleW, this.paddleH, 'white');
+        this.paddle(this.cvs.width - this.paddleW, this.p2y, this.paddleW, this.paddleH, 'white');
+        this.ball(this.bx, this.by, 10, 'white');
+        this.ctx.fillText(this.score1p, 100, 100);
+        this.ctx.fillText(this.score2p, this.cvs.width - 100, 100);
     }
 
-    static ball(x, y, r, color) {
-        ctx.fillStyle = color;
-        ctx.beginPath();
-        ctx.arc(x, y, r, 0, Math.PI * 2, true);
-        ctx.fill();
+    ball(x, y, r, color) {
+        this.ctx.fillStyle = color;
+        this.ctx.beginPath();
+        this.ctx.arc(x, y, r, 0, Math.PI * 2, true);
+        this.ctx.fill();
     }
 
-    static paddle(x, y, w, h, color) {
-        ctx.fillStyle = color;
-        ctx.fillRect(x, y, w, h);
+    paddle(x, y, w, h, color) {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x, y, w, h);
     }
 
-    static resetBall() {
-        if (score1p >= WINNING_SCORE || score2p >= WINNING_SCORE) {
-            showingWinScreen = true;
+    resetBall() {
+        if (this.score1p >= this.scoreMax || this.score2p >= this.scoreMax) {
+            this.gameSet = true;
         }
 
-        bx = cvs.width / 2;
-        by = cvs.height / 2;
-        vx = -vx;
+        this.bx = this.cvs.width / 2;
+        this.by = this.cvs.height / 2;
+        this.vx = -this.vx;
     }
-    static cpu() {
-        // 100をthis.paddleHにするとエラー
-        var p2yCentre = p2y + (100 / 2);
-        if (p2yCentre < by - 35) {
-            p2y += 6;
-        } else if (p2yCentre > by + 35) {
-            p2y -= 6;
+    cpu() {
+        var p2yCentre = this.p2y + (this.paddleH / 2);
+        if (p2yCentre < this.by - 35) {
+            this.p2y += 6;
+        } else if (p2yCentre > this.by + 35) {
+            this.p2y -= 6;
         }
     }
     mousePos(evt) {
-        var rect = cvs.getBoundingClientRect();
+        var rect = this.cvs.getBoundingClientRect();
         var root = document.documentElement;
         this.mouseX = evt.clientx - rect.left - root.scrollLeft;
         this.mouseY = evt.clientY - rect.top - root.scrollTop;
     }
 
     mouseClick(evt) {
-        if (showingWinScreen) {
-            score1p = 0;
-            score2p = 0;
-            showingWinScreen = false;
+        if (this.gameSet) {
+            this.score1p = 0;
+            this.score2p = 0;
+            this.gameSet = false;
         }
     }
+    play(pone) {
+        this.cvs = document.getElementById('pone');
+        this.ctx = this.cvs.getContext('2d');
+        var fps = 30;
+        setInterval(function () {
+            pone.move();
+            pone.draw();
+        }, 1000 / fps);
 
+        this.cvs.addEventListener('mousemove', function (evt) {
+            pone.mousePos(evt);
+            p1y = pone.mouseY - (pone.paddleH / 2);
+        });
+
+        this.cvs.addEventListener('mousedown', pone.mouseClick);
+    }
 }
 
 window.onclick = function () {
-    cvs = document.getElementById('pone');
-    ctx = cvs.getContext('2d');
-    const pone = new Pone();
-    var fps = 30;
-    setInterval(function () {
-        pone.move();
-        pone.draw();
-    }, 1000 / fps);
-
-    cvs.addEventListener('mousemove', function (evt) {
-        pone.mousePos(evt);
-        p1y = pone.mouseY - (pone.paddleH / 2);
-    });
-
-    cvs.addEventListener('mousedown', pone.mouseClick);
+    var pone = new Pone();
+    pone.play(pone);
 }
