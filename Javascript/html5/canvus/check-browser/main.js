3d28dd1e17ca13201764e344fc4d11ebdec80007
PrinzEugen7 <nsi05162@pooae.com>
2017-04-09 10:14:44 +0900

    Create main.js

---
+++ Javascript/html5/canvus/check-browser/main.js
@@ -0,0 +1,7 @@
+function main(){
+        cvs = document.getElementById("disp");
+        if (!cvs || !cvs.getContext) {
+                alert("このブラウザはHTML5に対応していません");
+                return false;
+        }
+}
