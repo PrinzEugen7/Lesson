c0917fc7fc9d78a41cdb862137bd8a18117d6f56
PrinzEugen7 <nsi05162@pooae.com>
2017-04-10 14:35:06 +0900

    Update map.py

--- Python/List/map.py
+++ Python/List/map.py
@@ -1,9 +1,15 @@
-def double(x):
+# 関数定義
+def func(x):
 	return x * 2
 
 data = [1, 2, 3, 4, 5]
+
 # 関数方式
-print( map(double, data) )                #=> [2, 4, 6] : 関数方式
+map1 = map(func, data)
 
 # lambda方式
-print( map(lambda x: x * 2, data) )       #=> [2, 4, 6] : 
+map2 = map(lambda x: x * 2, data)
+
+# 結果表示
+print( list(map1) ) # [2, 4, 6, 8, 10]
+print( list(map2) ) # [2, 4, 6, 8, 10]
