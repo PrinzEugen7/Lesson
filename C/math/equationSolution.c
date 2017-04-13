ea9aaf92c5cfe4f32c3f76f3414a6c248758898d
PrinzEugen <nsi05162@pooae.com>
2017-04-13 13:09:19 +0900

    Update equationSolution.c

--- C/math/equationSolution.c
+++ C/math/equationSolution.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
-// 計算する関数
-double f(double x)
+void equation2d(double a,double b,double c, 
+            double *re1, double *im1, double *im2, double *re2, double *D)
 {
-    return x*x*x-3*x*x+9*x-8;
+
+        *D = b*b-4.0*a*c;                      
+    
+        if(D>0) {                           
+                *re1 = (-b + sqrt(*D))/(2.0*a);
+                *re2 = (-b - sqrt(*D))/(2.0*a);
+                *im1 = 0.0;
+                *im2 = 0.0;
+        }
+        else if(D==0) {
+                *re1 = -b/(2.0*a);
+                *re2 = 0.0;
+                *im1 = 0.0;
+                *im2 = 0.0;
+        }
+        else {                            
+                *re1 = -b/(2.0*a);
+                *re2 = *re1;
+                *im1 = sqrt(-*D)/(2.0*a);
+                *im2 = -*im1;
+        }
+
+
 }
-
-// 2分法
-void bisection(double a, double b, double eps, double *solution, int *N)
-{
-    int i = 0;
-    double s;
-
-    // 解が収束条件を満たせば終了
-    while (!(fabs(a-b)<eps)){
-        i++;
-        s = (a+b)/2.0;
-        if(f(s) * f(a)<0) b=s;
-        else a = s;
-        if(i==1000) break; // 1000回繰り返したら強制終了
-    };
-    *N = i; 
-    *solution = s; 
-}
-
-
 
 int main()
 {
-    double solution;
-    int N;
-    // 2分法
-    bisection(0.0, 2.0, 1.0e-5, &solution, &N);
+        double re1,im1,re2,im2,D;
 
-    printf("解:%f (繰り返し回数：%d )",solution,N); // 解:1.165901 (繰り返し回数：18 )
+        // x^2 + 2x + 1 = 0 
+        equation2d(1,2,1,&re1,&im1,&re2,&im2,&D);
+        if(D==0) {
+                printf("%f + %f j\n",re1,im1); // -1.000000 + 0.000000 j
+        }
+        else {
+                printf("%f + %f j\n",re1,im1);
+                printf("%f + %f j\n",re2,im2);
+        }
 
-    return 0;
+        return 0;
 }
