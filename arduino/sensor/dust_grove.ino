fbe2358e1bfa1690c7e49de223066e6bc402fb0e
PrinzEugen <nsi05162@pooae.com>
2017-04-14 19:55:54 +0900

    Create dust_grove.ino

---
+++ arduino/sensor/dust_grove.ino
@@ -0,0 +1,27 @@
+int pin = 8;
+unsigned long t0;
+unsigned long ts = 30000; // 30000ms
+unsigned long lowOc = 0;
+float ratio = 0;
+float concentration = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(8,INPUT);
+  starttime = millis();//get the current time;
+}
+
+void loop() {
+  // LOWの占有率
+  lowOccupancy += pulseIn(pin, LOW);
+
+  // サンプリング
+  if ((millis() - t0) > ts){
+    // LOWの割合
+    ratio = lowOc/(ts*10.0);
+    // ほこりの濃度を算出
+    concent = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
+    Serial.print(lowOc+","+ratio+","+concent);
+    lowOc = 0;
+    t0 = millis();
+}
