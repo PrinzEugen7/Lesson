236ffb578219e3b6f59e75dee70dc133474899e7
PrinzEugen <nsi05162@pooae.com>
2017-04-14 17:53:02 +0900

    Update led.py

--- Python/raspi3/gpio/led.py
+++ Python/raspi3/gpio/led.py
@@ -4,18 +4,22 @@ import RPi.GPIO as GPIO
 import time
 
 def main():
-        pin_num = 14
-        GPIO.setmode(GPIO.BCM)          # GPIO番号で指定
-        GPIO.setup(pin_num, GPIO.OUT)        # GPIO25を出力用に設定
-        GPIO.output(pin_num, GPIO.HIGH)      # 点灯
-        time.sleep(2)                       # 2秒待機
-        GPIO.output(pin_num, GPIO.LOW)       # 消灯
-        time.sleep(2)               # 2秒待機
-        GPIO.output(pin_num, GPIO.HIGH)      # 点灯
-        time.sleep(2)                       # 2秒待機
-        GPIO.output(pin_num, GPIO.LOW)       # 消灯
-        time.sleep(2)                       # 2秒待機
-        GPIO.cleanup()                      # GPIOピンの設定解除
+    # 使用する出力ピン(GPIO)
+    pin_num = 14
+        
+    　# GPIOピンの定義
+　　　　GPIO.setmode(GPIO.BCM)          # GPIO番号で指定
+　　　　GPIO.setup(pin_num, GPIO.OUT)   # 出力モード
+    
+     # 点滅を5セット繰り返し
+　　　　for x in range(10):
+　　　　　　　　GPIO.output(pin_num, GPIO.HIGH) # 点灯
+　　　　　　　　time.sleep(2)                   # 2秒待機
+　　　　　　　　GPIO.output(pin_num, GPIO.LOW)  # 消灯
+　　　　　　　　time.sleep(2)                   # 2秒待機
+        
+    # GPIOピンの設定解除        
+　　　　GPIO.cleanup()                      
 
 if __name__ == "__main__":
     main()
