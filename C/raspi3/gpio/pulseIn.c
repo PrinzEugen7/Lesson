accdc4548c45ad0c4d50f2febe8264ee0b705c9f
PrinzEugen <nsi05162@pooae.com>
2017-04-17 20:14:56 +0900

    Create pulseIn.c

---
+++ C/raspi3/gpio/pulseIn.c
@@ -0,0 +1,37 @@
+int pulseIn(int pin, int level, int timeout)
+{
+   struct timeval tn, t0, t1;
+
+   long micros;
+
+   gettimeofday(&t0, NULL);
+
+   micros = 0;
+
+   while (digitalRead(pin) != level)
+   {
+      gettimeofday(&tn, NULL);
+
+      if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+      micros += (tn.tv_usec - t0.tv_usec);
+
+      if (micros > timeout) return 0;
+   }
+
+   gettimeofday(&t1, NULL);
+
+   while (digitalRead(pin) == level)
+   {
+      gettimeofday(&tn, NULL);
+
+      if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+      micros = micros + (tn.tv_usec - t0.tv_usec);
+
+      if (micros > timeout) return 0;
+   }
+
+   if (tn.tv_sec > t1.tv_sec) micros = 1000000L; else micros = 0;
+   micros = micros + (tn.tv_usec - t1.tv_usec);
+
+   return micros;
+}
