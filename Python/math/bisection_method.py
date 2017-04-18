247f22d3441f42af30e013ab26a484cb2e21764f
PrinzEugen <nsi05162@pooae.com>
2017-04-18 11:48:00 +0900

    Update bisection_method.py

--- Python/math/bisection_method.py
+++ Python/math/bisection_method.py
@@ -4,24 +4,23 @@ import math
 def f(x):
    return x*x*x-3*x*x+9*x-8;
  
-# ニュートン法
+# 二分法
 def bisection_method(a, b, eps):
     s = 0
     for i in range(1000):
-    	# 収束条件を満たせば終了
-    	if math.abs(a-b)<eps: break
+        # 収束条件を満たせば終了
+        if(abs(a-b)<eps): break
         # 漸化式
-        s = (a+b)/2.0;
+        s = (a+b)/2.0
         # 収束条件(近似解の変化が十分小さい)を満たせば計算終了
-        if(f(s) * f(a)<0): b=s
+        if(f(s) * f(a)<0): b = s
         else: a = s
-    return a, i
+    return s
     
     
 def main():
-    a , i = bisection_method(0.0, 2.0, 0.0001)
-    print("解:",a ,"(計算回数:", i+1, ")") # 解: 2.00000... (計算回数: 5 )
+    a = bisection_method(0.0, 2.0, 0.0001) # 1.16595458984375
+    print(a)
     
 if __name__ == '__main__':
     main()
-
