301bf7ecb20499169937b41d3983dbfc2099abb6
PrinzEugen <nsi05162@pooae.com>
2017-04-19 16:25:12 +0900

    Update euler.c

--- C/math/euler.c
+++ C/math/euler.c
@@ -10,17 +10,27 @@ double dxdt( double x)
     return (e-x)/r/c;
 }
 
-int main(void)
+// オイラー法(初期条件x0,  区間[t0, tn])
+double eulerMethod(double x0, double t0, double tn, int n)
 {
     int i, n;
     double x, t, h;
-    double x0=0, t0=0, tn=1;
     x = x0;
     t = t0;
     h = (tn - t0) /n;
+    
+    // 漸化式を計算
     for ( i=1; i <= n ; i++){
         x += dxdt(x) * h;
         t = t0 + i*h;
     }
+    return x;
+}
+
+int main(void)
+{
+    double xn;
+    xn = eulerMethod(0, 0, 1, 100);
+    printf("%f", xn)
     return 0;
 }
