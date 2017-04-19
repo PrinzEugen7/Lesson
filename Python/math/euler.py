7db61cc68f5625f412447e4e200986322088a7f2
PrinzEugen <nsi05162@pooae.com>
2017-04-19 17:46:28 +0900

    Update euler.py

--- Python/math/euler.py
+++ Python/math/euler.py
@@ -13,18 +13,16 @@ def euler(x0, t0, tn, n):
     t = t0
     h = (tn - t0) /n
     X = []
-    // 漸化式を計算
+    # 漸化式を計算
     for i in range(n):
         x += dxdt(x) * h
-        X.append[x]
+        X.append(x)
         t = t0 + i*h
     return X
-    
     
 def main():
     X = euler(0.0, 0.0, 1.0, 100)
     print(X)
-   
     
 if __name__ == '__main__':
     main()
