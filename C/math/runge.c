4ca02b7c75ad557b18cda74f562bdb6e0023009d
PrinzEugen <nsi05162@pooae.com>
2017-04-20 18:50:18 +0900

    Create runge.c

---
+++ C/math/runge.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double dxdt( double x)
+{
+    double c = 0.001;
+    double r = 100;
+    double e = 10;
+    return (e-x)/(r*c);
+}
+
+// オイラー法(初期条件x0,  区間[t0, tn])
+double runge(double x0, double t0, double tn, int n)
+{
+    int i;
+    double x, t, h, d1, d2, d3, d4;
+    x = x0;
+    t = t0;
+    h = (tn - t0) /n;
+    
+    // 漸化式を計算
+    for ( i=1; i <= n ; i++){
+        t = t0 + i*h;
+        d1 = dxdt(x);
+        d2 = dxdt(d1*h*0.5);
+        d3 = dxdt(d2*h*0.5);
+        d4 = dxdt(d3*h);
+        x += (h/6.0) * (d1 + 2 * d2 + 2 * d3 + d4); 
+        printf("x(%f)=%f\n", t, x);
+    }
+    return x;
+}
+
+int main(void)
+{
+    runge(0, 0, 1, 100);
+    return 0;
+}
+
