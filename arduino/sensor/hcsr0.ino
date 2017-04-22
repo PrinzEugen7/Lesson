e6881bc829581383ebaa85912149a8b1a1545808
PrinzEugen <nsi05162@pooae.com>
2017-04-22 11:47:44 +0900

    Create hcsr0.ino

---
+++ arduino/sensor/hcsr0.ino
@@ -0,0 +1,27 @@
+int trig = 8; // 出力ピン
+int echo = 9; // 入力ピン
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  }
+
+void loop() {
+  // 超音波の出力終了
+  digitalWrite(trig,LOW);
+  delayMicroseconds(1)
+  // 超音波を出力;
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(11);
+  // 超音波を出力終了
+  digitalWrite(trig,LOW);
+  // 出力した超音波が返って来る時間を計測
+  int t = pulseIn(echo,HIGH);
+  // 計測した時間と音速から反射物までの距離を計算
+  float distance = t*0.017;
+  // 計算結果をシリアル通信で出力
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+}
