df73750dda2c8f79314f461de9f8524c734c01da
PrinzEugen <nsi05162@pooae.com>
2017-04-22 23:46:06 +0900

    Update hcsr04.py

--- Python/raspi3/gpio/hcsr04.py
+++ Python/raspi3/gpio/hcsr04.py
@@ -3,37 +3,39 @@
 import RPi.GPIO as GPIO
 import time
 
-def pulseIn(pin, start):
-    if start==1: end = 0
-    else: end = 1
-    while GPIO.input(ECHO_PIN) == end:
-        t_start = time.time()
-        
-    while GPIO.input(ECHO_PIN) == start:
-        t_end = time.time()
-
-    return t_end - t_start
-
-def calc_distance(TRIG_PIN, ECHO_PIN, num, v=17000):
+def calc_distance(TRIG_PIN, ECHO_PIN, num, v=34000):
+    # ピン番号をGPIOで指定
     GPIO.setmode(GPIO.BCM)
+    # TRIG_PINを出力, ECHO_PINを入力
     GPIO.setup(TRIG_PIN,GPIO.OUT)
     GPIO.setup(ECHO_PIN,GPIO.IN)
     GPIO.setwarnings(False)
+    
     while(num):
-
+        # TRIGピンを0.3[s]だけLOW
         GPIO.output(TRIG_PIN, GPIO.LOW)
         time.sleep(0.3)
-        
+        # TRIGピンを0.00001[s]だけ出力(超音波発射)        
         GPIO.output(TRIG_PIN, True)
         time.sleep(0.00001)
         GPIO.output(TRIG_PIN, False)
-        t = pulseIn(TRIG, 1)
-        distance = v * t
+        # ECHO_PINがHIGHである時間を計測
+        while GPIO.input(ECHO_PIN) == 0:
+            t_start = time.time()
+        
+        while GPIO.input(ECHO_PIN) == 1:
+            t_end = time.time()
+
+        t = t_end - t_start
+        # 距離[cm] = 音速[cm/ms] * 時間[ms]/2
+        distance = v * t/2
         print(distance, "cm")
+    # ピン設定解除
     GPIO.cleanup()
         
 def main():
-    calc_distance(14, 15, 10)
+    # 距離計測(TRIGピン番号, ECHO_PIN番号, 計測回数, 音速[cm/ms])
+    calc_distance(14, 15, 10, 34000)
 
         
 if __name__ == "__main__":
