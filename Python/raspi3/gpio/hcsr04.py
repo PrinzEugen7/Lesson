6523542a36ab3f39104e6bcc3337702c7dc885f6
PrinzEugen <nsi05162@pooae.com>
2017-04-22 22:28:01 +0900

    Update hcsr04.py

--- Python/raspi3/gpio/hcsr04.py
+++ Python/raspi3/gpio/hcsr04.py
@@ -4,14 +4,13 @@ import RPi.GPIO as GPIO
 import time
 
 def main():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
     TRIG = 14
     ECHO = 15
-    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)    
     while(1):
-        GPIO.setup(TRIG,GPIO.OUT)
-        GPIO.setup(ECHO,GPIO.IN)
+
         GPIO.output(TRIG, GPIO.LOW)
         time.sleep(0.3)
         
@@ -28,7 +27,11 @@ def main():
         timepassed = signalon - signaloff
         distance = timepassed * 17000
         print(distance, "cm")
-
+        c = sys.stdin.read(1)
+        if c == 's':
+            # GPIOピンの設定解除        
+            GPIO.cleanup()   
+            break
         
 if __name__ == "__main__":
     main()
