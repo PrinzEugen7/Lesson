3347bda8cc4878876fa4d155aa3ff243692c0d58
PrinzEugen <nsi05162@pooae.com>
2017-04-22 10:28:06 +0900

    Create hc_sr04.py

---
+++ Python/raspi3/gpio/hc_sr04.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+
+def main():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    TRIG = 11
+    ECHO = 13
+    
+    while(1):
+        GPIO.setup(TRIG,GPIO.OUT)
+        GPIO.setup(ECHO,GPIO.IN)
+        GPIO.output(TRIG, GPIO.LOW)
+        time.sleep(0.3)
+        
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+          signaloff = time.time()
+        
+        while GPIO.input(ECHO) == 1:
+          signalon = time.time()
+
+        timepassed = signalon - signaloff
+        distance = timepassed * 17000
+        return(distance, "cm")
+        if c == 's':
+            GPIO.cleanup()
+            break
+        
+if __name__ == "__main__":
+    main()
