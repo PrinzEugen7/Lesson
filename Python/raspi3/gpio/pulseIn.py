7f6b10f90fb389f13ae3e1daa601624821e79a90
PrinzEugen <nsi05162@pooae.com>
2017-04-23 18:25:58 +0900

    Update pulseIn.py

--- Python/raspi3/gpio/pulseIn.py
+++ Python/raspi3/gpio/pulseIn.py
@@ -1,6 +1,8 @@
 # HIGH or LOWの時計測
 def pulseIn(PIN, start=1, end=0):
     if start==0: end = 1
+    t_start = 0
+    t_end = 0
     # ECHO_PINがHIGHである時間を計測
     while GPIO.input(PIN) == end:
         t_start = time.time()
