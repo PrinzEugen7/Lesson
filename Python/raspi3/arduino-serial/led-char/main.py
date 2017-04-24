6959dc1a40b8416b98903f7d84da8305d2ef06ba
PrinzEugen <nsi05162@pooae.com>
2017-04-24 20:17:38 +0900

    Update main.py

--- Python/raspi3/arduino-serial/led-char/main.py
+++ Python/raspi3/arduino-serial/led-char/main.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import serial
 
-com_num = 6 # Arduinoを繋いでるCOMポート番号
-
 def main():
-    ser = serial.Serial(com_num-1, 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     while True:
         flag = raw_input()
         ser.write(flag)
