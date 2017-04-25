65e5fe1f519350903d2d9c525668903d1ca1d3f4
PrinzEugen <nsi05162@pooae.com>
2017-04-25 12:11:44 +0900

    Create main.py

---
+++ Python/raspi3/arduino-serial/led-string/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import serial
+
+def main():
+
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    while True:
+        flag = raw_input()
+        ser.write(flag+".")
+        if(flag == 'a'): # aが入力されたら通信終了
+            break;
+    ser.close()
+
+if __name__ == '__main__':
+    main()
