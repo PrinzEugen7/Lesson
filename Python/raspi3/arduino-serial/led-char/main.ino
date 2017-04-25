6a102199f8cca54a7db7f9ee30079a2b7fcbd8ef
PrinzEugen <nsi05162@pooae.com>
2017-04-25 20:25:07 +0900

    Update main.ino

--- Python/raspi3/arduino-serial/led-char/main.ino
+++ Python/raspi3/arduino-serial/led-char/main.ino
@@ -1,32 +1,17 @@
+int cmd = 0;
 
-
-void setup(){
-  pinMode(13, OUTPUT);
-  // シリアルポートを9600 bps[ビット/秒]で初期化
+void setup() {
   Serial.begin(9600);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
 }
 
-void loop(){
-  
-  int inputchar;
-  // シリアルポートより1文字読み込む
-  inputchar = Serial.read();
- 
-  if(inputchar != -1 ){
-    // 読み込んだデータが -1 以外の場合　以下の処理を行う 
-    switch(inputchar){
-      case 's':
-        // 読み込みデータが　o の場合
-        digitalWrite(13, HIGH);
-        break;
-      case 'q': 
-        // 読み込みデータが　p の場合
-        digitalWrite(13, LOW);
-        break;
-    }
+void loop() {
+  if (Serial.available() > 0) {
+    cmd = Serial.read();
+    // 入力値がoなら点灯, それ以外は消灯
+    if(cmd=='o') digitalWrite(13, HIGH);
+    else digitalWrite(13, LOW);
   }
-  else {
-    // 読み込むデータが無い場合は何もしない
-  }
+  delay(10);
 }
-
