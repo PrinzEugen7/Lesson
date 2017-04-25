dfd0de85d5d8755da4d4c114871f17bf5af97e9d
PrinzEugen <nsi05162@pooae.com>
2017-04-25 20:57:53 +0900

    Update main.py

--- Python/raspi3/arduino-serial/led-string/main.py
+++ Python/raspi3/arduino-serial/led-string/main.py
@@ -5,8 +5,12 @@ def main():
 
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     while True:
-        flag = raw_input()
-        ser.write(flag+".")
+        flag = input()
+        flag = flag + "\n"
+        ser.write(flag.encode())
+        # python2 ver
+        #flag = raw_input()
+        #ser.write(flag+"\n")
         if(flag == 'a'): # aが入力されたら通信終了
             break;
     ser.close()
