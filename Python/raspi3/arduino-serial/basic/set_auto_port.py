4ddbed2e5920b57d9a5f9669a4e98d25aa0ee1df
PrinzEugen <nsi05162@pooae.com>
2017-04-25 19:14:53 +0900

    Create set_auto_port.py

---
+++ Python/raspi3/arduino-serial/basic/set_auto_port.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import serial
+import serial.tools.list_ports
+
+def main():
+    ser = serial.Serial(list(serial.tools.list_ports.comports())[0][0], 9600, timeout=1)
+    while True:
+        flag =　input()
+        ser.write(flag.encode())
+        # ser.write(flag) # python2
+        if(flag == 'a'): # aが入力されたら通信終了
+            break;
+    ser.close()
+
+if __name__ == '__main__':
+    main()
