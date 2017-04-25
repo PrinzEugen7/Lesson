cc0813ae7e1404e00e8bcc552c0b8d87c775e2ca
PrinzEugen <nsi05162@pooae.com>
2017-04-25 21:49:41 +0900

    Update main.ino

--- Python/raspi3/arduino-serial/led-string/main.ino
+++ Python/raspi3/arduino-serial/led-string/main.ino
@@ -1,5 +1,5 @@
 char data[10];   // 文字列格納用
-int i = 0;  // 文字数のカウンタ
+
 
 void setup() {
   Serial.begin(9600);
@@ -9,10 +9,12 @@ void setup() {
 
 // データ受信
 String serialRead(){
+  int i = 0;  // 文字数のカウンタ
   // データ受信したとき
   if (Serial.available()) {
+    // 1文字ずつ読み込み
     data[i] = Serial.read();
-     // 文字数が30以上 or 終端文字
+    // 文字数が10以上 or 終端文字なら終了
     if (i > 10 || data[i] == '\0') i = 0;
     else { i++; }
   }
