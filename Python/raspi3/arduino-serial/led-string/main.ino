a71bd14a0ad660cf9e5c991e3f4f38f8a79fbbd1
PrinzEugen <nsi05162@pooae.com>
2017-04-25 21:43:11 +0900

    Update main.ino

--- Python/raspi3/arduino-serial/led-string/main.ino
+++ Python/raspi3/arduino-serial/led-string/main.ino
@@ -1,25 +1,29 @@
-#include <String.h>
-
-String inputStr = String(20);
+char data[30];   // 文字列格納用
+int i = 0;  // 文字数のカウンタ
 
 void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  inputStr = "";
+  digitalWrite(13, LOW);
 }
 
-void serialRead() {
-  while (Serial.available() > 0) {
-    char inputChar = Serial.read();
-    if (inputChar == '\n') break;
-    if (inputStr.length() > 20) break;
-    inputStr.concat(String(inputChar));
+// データ受信
+String serialRead(){
+  // データ受信したとき
+  if (Serial.available()) {
+    data[i] = Serial.read();
+     // 文字数が30以上 or 終端文字
+    if (i > 30 || data[i] == '\0') {
+      i = 0;      // カウンタの初期化
+      break;
+    }
+    else { i++; }
   }
+  return String(data);
 }
+
 void loop() {
-  serialRead();
-  if (inputStr.startsWith("on")) digitalWrite(13, HIGH);
-  else if (inputStr.startsWith("off")) digitalWrite(13, LOW);
-  inputStr = "";
+  String cmd = serialRead();
+  if(cmd =="on") digitalWrite(13, HIGH);
+  if(cmd =="off") digitalWrite(13, LOW);
 }
