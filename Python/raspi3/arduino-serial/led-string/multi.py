72c7feaec331496bcdc0854c70dec2639128e394
PrinzEugen <nsi05162@pooae.com>
2017-04-26 00:42:03 +0900

    Update multi.py

--- Python/raspi3/arduino-serial/led-string/multi.py
+++ Python/raspi3/arduino-serial/led-string/multi.py
@@ -2,17 +2,16 @@
 import serial
 
 def main():
-
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     while True:
-        flag = input()
-        flag = flag + "\0"
-        ser.write(flag.encode())
+        cmd = input()
+        cmd = cmd + "\0"
+        ser.write(cmd.encode())
         # python2 ver
         #flag = raw_input()
         #ser.write(flag+"\n")
         if(flag == 'q'): # qが入力されたら通信終了
-            break;
+            break
     ser.close()
 
 if __name__ == '__main__':
