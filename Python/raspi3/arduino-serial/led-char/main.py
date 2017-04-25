9f8c4dd235891099c3c2fc67fff8126707274ba3
PrinzEugen <nsi05162@pooae.com>
2017-04-25 20:26:42 +0900

    Update main.py

--- Python/raspi3/arduino-serial/led-char/main.py
+++ Python/raspi3/arduino-serial/led-char/main.py
@@ -4,8 +4,11 @@ import serial
 def main():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     while True:
-        flag =　input()
+        flag = input()
         ser.write(flag.encode())
+        # python2の場合
+        #flag =　raw_input()
+        #ser.write(flag)
         if(flag == 'a'): # aが入力されたら通信終了
             break;
     ser.close()
