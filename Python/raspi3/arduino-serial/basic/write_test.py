ded4096cb8d2326cba901cfaafa5b41803b25f98
PrinzEugen <nsi05162@pooae.com>
2017-04-25 18:36:23 +0900

    Create write_test.py

---
+++ Python/raspi3/arduino-serial/basic/write_test.py
@@ -0,0 +1,18 @@
+
+
+# -*- coding: utf-8 -*-
+import serial
+
+def main():
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    while True:
+        flag =　input()
+        ser.write(flag.encode())
+        # ser.write(flag) # python2
+        if(flag == 'a'): # aが入力されたら通信終了
+            break;
+    ser.close()
+
+if __name__ == '__main__':
+    main()
+
