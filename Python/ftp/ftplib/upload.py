77755ddf85ad2f2e920f4ad97f0b793fc760c342
PrinzEugen <nsi05162@pooae.com>
2017-04-26 16:38:23 +0900

    Update upload.py

--- Python/ftp/ftplib/upload.py
+++ Python/ftp/ftplib/upload.py
@@ -5,12 +5,12 @@ def main():
     # 接続先サーバーのホスト名 or IP adress
     ftp = ftplib.FTP("xxx.xxx.xxx")
     ftp.set_pasv("true")
-    # ユーザIDとパスワードを指定しログイン
+    # ユーザ名とパスワードを指定しログイン
     ftp.login("user", "password")
     # アップロードするファイル
-    fp = open("test.csv")
+    fp = open("test.csv", 'rb')
     # アップロード先のディレクトリ
-    ftp.storbinary("STOR /files/test.csv",fp)
+    ftp.storbinary("STOR /sample/test.csv",fp)
     # 終了処理
     ftp.close()
     fp.close()
