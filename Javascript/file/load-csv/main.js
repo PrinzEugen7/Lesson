e2b379eaea68e00efcf399988df3516b3a5e9f5d
PrinzEugen <nsi05162@pooae.com>
2017-04-28 22:41:32 +0900

    Create main.js

---
+++ Javascript/file/load-csv/main.js
@@ -0,0 +1,22 @@
+// CSVファイル読み込み
+function csvToArray(path) {
+	var csvData = new Array();
+	var data = new XMLHttpRequest();	
+	data.open("GET", path, false);
+	data.send(null);
+	var LF = String.fromCharCode(10);
+	var lines = data.responseText.split(LF);
+	for (var i = 0; i < lines.length;++i) {
+		var cells = lines[i].split(",");
+		if( cells.length != 1 ) {
+			csvData.push(cells);
+		}
+	}
+	return csvData;
+}
+
+// ページロード時に実行
+window.onload=function () {
+    var data = csvToArray("data.csv");
+    alert(data[0]);
+};
