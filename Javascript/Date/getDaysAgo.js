2dffabafc311561aadf6e2762886a4fe87057706
PrinzEugen <nsi05162@pooae.com>
2017-04-29 20:36:39 +0900

    Create getDaysAgo.js

---
+++ Javascript/Date/getDaysAgo.js
@@ -0,0 +1,14 @@
+function getDate(day) {
+  var date = new Date();
+  date.setDate(date.getDate() + day);
+  var year  = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day   = date.getDate();
+  return String(year) + "-" + String(month) + "-" + String(day);
+}
+
+// ページロード時に実行
+window.onload = function () {
+  alert(getDate(-7));
+};
+
