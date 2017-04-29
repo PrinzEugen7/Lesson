2b67bbf82d2a75901333247ff77bb7b7791fcf46
PrinzEugen <nsi05162@pooae.com>
2017-04-29 20:35:35 +0900

    Create getYesterday.js

---
+++ Javascript/Date/getYesterday.js
@@ -0,0 +1,13 @@
+function getDate(day) {
+  var date = new Date();
+  date.setDate(date.getDate() + day);
+  var year  = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day   = date.getDate();
+  return String(year) + "-" + String(month) + "-" + String(day);
+}
+
+// ページロード時に実行
+window.onload = function () {
+  alert(getDate(-1));
+};
