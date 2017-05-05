acd10190bd99aee05b1e44bf7c9b1f6ce38d9715
PrinzEugen <nsi05162@pooae.com>
2017-05-05 18:04:15 +0900

    Create getDateZeroume.js

---
+++ Javascript/Date/getDateZeroume.js
@@ -0,0 +1,13 @@
+function getDate(day) {
+  var date = new Date();
+  date.setDate(date.getDate() + day);
+  var year  = date.getFullYear();
+  var month = ("0"+(date.getMonth() + 1)).slice(-2);
+  var day   = ("0"+date.getDate()).slice(-2)
+  return String(year) + "-" + String(month) + "-" + String(day);
+}
+
+// ページロード時に実行
+window.onload = function () {
+  alert(getDate(0)); // 2017-05-05
+};
