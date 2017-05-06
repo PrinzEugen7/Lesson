27cb55ec83b2b8961ea88bd4ad562dee5d8a1509
PrinzEugen <nsi05162@pooae.com>
2017-05-06 17:28:44 +0900

    Create update.py

---
+++ Python/file/csv/update.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import csv
+
+def read_csv(filename):
+    f = open(filename, "r")
+    csv_data = csv.reader(f)
+    list = [ e for e in csv_data]
+    f.close()
+    return list
+    
+def update_list2d(list, data):
+    for i in range(len(list)):
+        if list[i][0]==data[0]: list[i] = data
+    return list
+
+def write_csv(filename, list):
+   with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(list)   
+
+   f.close()
+
+def main():
+    filename = 'data.csv'
+    csv_data = read_csv(filename)
+    data = ["2017-01-01 01:00", 20, 50, 1030, 5]
+    csv_data2 = update_list2d(csv_data, data)
+    write_csv(csv_data2, filename)
+    
+if __name__=='__main__':
+    main()
