24acda99bad9d98bce4cea459c35c11af1032304
PrinzEugen <nsi05162@pooae.com>
2017-05-06 21:58:00 +0900

    Create cls.py

---
+++ Python/os/sytem/cls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+
+def main():
+    for i in range(10):
+        print(i)
+        time.sleep(1)
+        os.system('cls')
+
+
+if __name__ == "__main__":
+    main()
