6faf6fc0339dabfc7c0d8360ebab12a552eb590d
PrinzEugen <nsi05162@pooae.com>
2017-05-24 23:33:10 +0900

    Rename sortAscendingNum.js to sortAscendingKatakana.js

---
+++ Javascript/array/sortAscendingKatakana.js
@@ -0,0 +1,20 @@
+function katakanaToHiragana(src) {
+	return src.replace(/[\u30a1-\u30f6]/g, function(match) {
+		var chr = match.charCodeAt(0) - 0x60;
+		return String.fromCharCode(chr);
+	});
+}
+
+function sortFunc(a, b){
+	a = katakanaToHiragana(a.toString());
+	b = katakanaToHiragana(b.toString());
+	if(a < b) return -1;
+	else if(a > b) return 1;
+	return 0;
+}
+
+window.onload = function () {
+    array = ["エミヤ", "ジャンヌ", "アルトリア"]
+    array.sort(sortFunc);
+    alert(array); // アルトリア,エミヤ,ジャンヌ
+}
