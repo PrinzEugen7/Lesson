3056c0fa7e846346c1672cf6330827222eafc725
PrinzEugen <nsi05162@pooae.com>
2017-06-23 20:59:33 +0900

    Create histgram.py

---
+++ Python/opencv/cv2/image/histgram.py
@@ -0,0 +1,22 @@
+#-*- coding:utf-8 -*-
+import cv2
+from matplotlib import pyplot as plt
+
+def main():
+    # 画像ファイルを8bitで読み込む
+    gray = cv2.imread('test.jpg', 0)
+    #　画像ヒストグラムを作成
+    plt.rcParams["font.family"] = "Times New Roman"
+    plt.hist(gray.ravel(),256,[0,256])
+    plt.xlim(0, 255)
+    plt.ylim(0, 600)
+    plt.xticks(fontsize = 17)
+    plt.yticks(fontsize = 17)
+    plt.xlabel('Pixel value', fontsize=20)
+    plt.ylabel('Number of pixels', fontsize=20)
+    plt.grid()
+    plt.show()
+
+    
+if __name__ == "__main__":
+    main()
