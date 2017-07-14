7140ca137d94be3d568939f35064dc8af92230d1
PrinzEugen <nsi05162@pooae.com>
2017-07-15 01:16:02 +0900

    Update prewitt.py

--- Python/opencv/image/filter/prewitt.py
+++ Python/opencv/image/filter/prewitt.py
@@ -3,13 +3,21 @@ import cv2
 import numpy as np
 
 def filter2d(src, kernel):
+    # カーネルサイズ
     m, n = kernel.shape
-    h = src.shape[0] - m + 1
-    w = src.shape[1] - m + 1
+    
+    # 畳み込み演算をしない領域の幅
+    d = int((m-1)/2)
+    h, w = src.shape[0], src.shape[1]
+    
+    # 出力画像用の配列（要素は全て0）
     dst = np.zeros((h,w))
-    for y in range(h):
-        for x in range(w):
+    
+    for y in range(d, h - d - 1):
+        for x in range(d, w - d - 1):
+            # 畳み込み演算
             dst[y][x] = np.sum(src[y:y+m, x:x+m]*kernel)
+            
     return dst
     
 def main():
