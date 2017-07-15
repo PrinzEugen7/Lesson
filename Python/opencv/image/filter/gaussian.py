6129480e1168f5857ac62e92ac62e34fcefe5f6e
PrinzEugen <nsi05162@pooae.com>
2017-07-15 11:09:45 +0900

    Create gaussian.py

---
+++ Python/opencv/image/filter/gaussian.py
@@ -0,0 +1,48 @@
+#-*- coding:utf-8 -*-
+import cv2
+import numpy as np
+
+def filter2d(src, kernel):
+    # カーネルサイズ
+    m, n = kernel.shape
+    
+    # 畳み込み演算をしない領域の幅
+    d = int((m-1)/2)
+    h, w = src.shape[0], src.shape[1]
+    
+    # 出力画像用の配列（要素は全て0）
+    dst = np.zeros((h,w))
+    
+    for y in range(d, h - d - 1):
+        for x in range(d, w - d - 1):
+            # 畳み込み演算
+            dst[y][x] = np.sum(src[y:y+m, x:x+m]*kernel)
+            
+    return dst
+    
+def main():
+    # 入力画像をグレースケールで読み込み
+    gray = cv2.imread("input.jpg", 0)
+    
+    # カーネル（縦方向の輪郭検出用）
+    kernel = np.array([[1/16, 1/8, 1/16],
+                              [1/8, 1/4, 1/8],
+                              [1/16, 1/8, 1/16]])
+
+    # 方法1
+    dst1 = filter2d(gray, kernel)
+    
+    # 方法2       
+    dst2 = cv2.filter2D(gray, -1, kernel)
+    
+    # 方法3
+    dst3 = cv2.GaussianBlur(gray, ksize=3,sigmaX=2)
+    
+    # 結果を出力
+    cv2.imwrite("output1.jpg", dst1)
+    cv2.imwrite("output2.jpg", dst2)
+    cv2.imwrite("output3.jpg", dst3)
+
+    
+if __name__ == "__main__":
+    main()
