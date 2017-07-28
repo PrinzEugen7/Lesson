ba513fbdfffb128cb12d93ae296bbf4bce26a16b
PrinzEugen <nsi05162@pooae.com>
2017-07-28 20:35:54 +0900

    Create adaptive.py

---
+++ Python/opencv/image/threshold/adaptive.py
@@ -0,0 +1,43 @@
+#-*- coding:utf-8 -*-
+import cv2
+import numpy as np
+
+def threshold(src, ksize=3, c=2):
+    
+    # 畳み込み演算をしない領域の幅
+    d = int((ksize-1)/2)
+    h, w = src.shape[0], src.shape[1]
+    
+    # 出力画像用の配列（要素は全て255）
+    dst = np.empty((h,w))
+    dst.fill(255)
+    
+    n = ksize**2
+
+    for y in range(0, h):
+        for x in range(0, w):
+            # 近傍の画素値の平均から閾値を求める
+            t = np.sum(src[y-d:y+d+1, x-d:x+d+1]) / n
+            # 求めた閾値で二値化処理
+            if(src[y][x] < t - c): dst[y][x] = 0
+            else: dst[y][x] = 255
+
+    return dst
+    
+def main():
+    # 入力画像をグレースケールで読み込み
+    gray = cv2.imread("input.jpg", 0)
+
+    # 方法1
+    dst1 = threshold(gray, ksize=11, c=2)
+    
+    # 方法2       
+    dst2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
+    
+    # 結果を出力
+    cv2.imwrite("output1.jpg", dst1)
+    cv2.imwrite("output2.jpg", dst2)
+
+    
+if __name__ == "__main__":
+ main()
