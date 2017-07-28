f4375e8065d36dc371de68bbd428329fc0fd2051
PrinzEugen <nsi05162@pooae.com>
2017-07-28 14:11:22 +0900

    Update median_filter.py

--- Python/opencv/image/filter/median_filter.py
+++ Python/opencv/image/filter/median_filter.py
@@ -2,33 +2,30 @@
 import cv2
 import numpy as np
 
-def median_filter(src, kernel):
-    # カーネルサイズ
-    m, n = kernel.shape
-    
+def median_filter(src, ksize):
     # 畳み込み演算をしない領域の幅
-    d = int((m-1)/2)
+    d = int((ksize-1)/2)
     h, w = src.shape[0], src.shape[1]
     
-    # 出力画像用の配列（要素は全て0）
-    dst = np.zeros((h,w))
+    # 出力画像用の配列（要素は入力画像と同じ）
+    dst = src.copy()
     
     for y in range(d, h - d - 1):
         for x in range(d, w - d - 1):
             # 畳み込み演算
-            dst[y][x] = np.sum(src[y:y+m, x:x+m]*kernel)
+            dst[y][x] = np.median(src[y:y+ksize, x:x+ksize])
             
     return dst
     
 def main():
     # 入力画像をグレースケールで読み込み
-    gray = cv2.imread("input.jpg", 0)
+    gray = cv2.imread("input.png", 0)
 
     # 方法1
-    dst1 = median_filter(gray, kernel)
+    dst1 = median_filter(gray, ksize=5)
     
     # 方法2       
-    dst2 = cv2.medianBlur(gray, ksize=3)
+    dst2 = cv2.medianBlur(gray, ksize=5)
     
     # 方法3
 
