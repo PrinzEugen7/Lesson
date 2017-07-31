bddd4c9579fb8b8f935d0a5e1ae7235510f7c240
PrinzEugen <nsi05162@pooae.com>
2017-07-31 15:01:42 +0900

    Update rgb_to_hsv.py

--- Python/opencv/image/color-space/rgb_to_hsv.py
+++ Python/opencv/image/color-space/rgb_to_hsv.py
@@ -2,41 +2,56 @@
 import cv2
 import numpy as np
 
-def rgb_to_hsv(src):
-    src = src.astype('float')
-    hsv = np.zeros_like(src)
-    hsv[..., 3:] = src[..., 3:]
-     # チャンネル分解
-    r, g, b = src[:,:,0], src[:,:,1], src[:,:,2]
-    maxc = np.max(src[..., :3], axis=-1)
-    minc = np.min(src[..., :3], axis=-1)
-    hsv[:,:,2] = maxc
-    mask = maxc != minc
-    hsv[mask, 1] = (maxc - minc)[mask] / maxc[mask]
-    rc = np.zeros_like(r)
-    gc = np.zeros_like(g)
-    bc = np.zeros_like(b)
-    rc[mask] = (maxc - r)[mask] / (maxc - minc)[mask]
-    gc[mask] = (maxc - g)[mask] / (maxc - minc)[mask]
-    bc[mask] = (maxc - b)[mask] / (maxc - minc)[mask]
-    hsv[:,:,0] = np.select(
-        [r == maxc, g == maxc], [bc - gc, 2.0 + rc - bc], default=4.0 + gc - rc)
-    hsv[:,:,0] = (hsv[:,:,0] / 6.0) % 1.0
-    return hsv 
-    
+# 膨張処理
+def rgb_to_hsv(src, ksize=3):
+    h, w, c = src.shape
+    dst = src.copy()
+    d = int((ksize-1)/2)
+
+    for y in range(0, h):
+        for x in range(0, w):
+            # R, G, Bの値を取得して0～1の範囲内にする
+            [b, g, r] = src[y][x]/255.0
+
+            # R, G, Bの値から最大値と最小値を計算
+
+            mx, mn = max(r, g, b), min(r, g, b)
+            # 最大値 - 最小値
+            diff = mx - mn
+
+            # Hの値を計算
+            if mx == mn : h = 0
+            elif mx == r : h = 60 * ((g-b)/diff)     
+            elif mx == g : h = 60 * ((b-r)/diff) + 120  
+            elif mx == b : h = 60 * ((r-g)/diff) + 240
+                
+            # Sの値を計算
+            if mx != 0:s = diff/mx       
+            else: s = 0
+            
+            # Vの値を計算
+            v = mx
+            
+            # H, S, Vの値を0～255の範囲内にして格納
+            dst[y][x] = [h, s * 255, v * 255]
+
+    return dst
+
+
 def main():
     # 入力画像の読み込み
     img = cv2.imread("input.jpg")
 
-    # 方法1
+    # 方法1(NumPyで実装)
     hsv1 = rgb_to_hsv(img)
     
-    # 方法2       
+    # 方法2(OpenCVで実装)       
     hsv2 = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     
     # 結果を出力
     cv2.imwrite("hsv1.jpg", hsv1)
     cv2.imwrite("hsv2.jpg", hsv2)
-    
+
+
 if __name__ == "__main__":
     main()
