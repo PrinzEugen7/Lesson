4e6309dc2cb29a687e6a820d3b90ab644363482f
PrinzEugen <nsi05162@pooae.com>
2017-07-31 23:31:02 +0900

    Update otsu.py

--- Python/opencv/image/threshold/otsu.py
+++ Python/opencv/image/threshold/otsu.py
@@ -2,38 +2,37 @@
 import cv2
 import numpy as np
 
-# 膨張処理
+# 大津の手法
 def threshold_otsu(gray, min_value=0, max_value=255):
 
-    # ヒストグラム算出
     hist = [np.sum(gray == i) for i in range(256)]
 
     s_max = (0,-10)
     ss = []
-    
     for th in range(256):
-        # update
-        w0 = sum(hist[:th])
-        w1 = sum(hist[th:])
+        
+        # クラス1とクラス2の平均値・画素数を計算
+        n1 = sum(hist[:th])
+        n2 = sum(hist[th:])
 
-        mu0 = sum([i * hist[i] for i in range(0,th)]) / w0 if w0 > 0 else 0       
-        mu1 = sum([i * hist[i] for i in range(th, 256)]) / w1 if w1 > 0 else 0
+        mu1 = sum([i * hist[i] for i in range(0,th)]) / n1 if n1 > 0 else 0       
+        mu2 = sum([i * hist[i] for i in range(th, 256)]) / n2 if n2 > 0 else 0
 
-        # calculate 
-        s = w0 * w1 * (mu0 - mu1) ** 2
-        ss.append(s)
+        # 分離度の分子を計算
+        s = n1 * n2 * (mu1 - mu2) ** 2
 
+        # 分離度の分子が最大のとき、分離度と閾値を記録
         if s > s_max[1]:
             s_max = (th, s)
-            
+    
+    # 分離度が最大のときの閾値を取得
     t = s_max[0]
 
-    # 算出した閾値で2値化
+    # 算出した閾値で二値化処理
     gray[gray<t] = min_value
     gray[gray>=t] = max_value
-    
-    return gray
 
+    return gray
 
 def main():
     # 入力画像の読み込み
@@ -42,10 +41,10 @@ def main():
     # グレースケール変換
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    # 方法1
-    th1 = threshold_otsu(gray, 0, 255)
+    # 方法1（NumPyで実装）
+    th1 = threshold_otsu(gray)
     
-    # 方法2       
+    # 方法2 （OpenCVで実装）      
     ret, th2 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)    
 
     # 結果を出力
