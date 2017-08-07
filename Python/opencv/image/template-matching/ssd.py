8e32af36c4e0975a656deca2d934ca1b2c1cd311
PrinzEugen <nsi05162@pooae.com>
2017-08-07 16:33:51 +0900

    Update ssd.py

--- Python/opencv/image/template-matching/ssd.py
+++ Python/opencv/image/template-matching/ssd.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np
 
-def template_matching_ssd(src, temp):
+def template_matching_sad(src, temp):
     # 画像の高さ・幅を取得
     h, w = src.shape
     ht, wt = temp.shape
@@ -13,8 +13,8 @@ def template_matching_ssd(src, temp):
     # 走査
     for dy in range(0, h - ht):
         for dx in range(0, w - wt):
-            #　二乗誤差の和を計算
-            diff = (src[dy:dy + ht, dx:dx + wt] - temp)**2
+            # 差分の絶対和を計算
+            diff = np.abs(src[dy:dy + ht, dx:dx + wt] - temp)
             score[dy, dx] = diff.sum()
 
     # スコアが最小の走査位置を返す
@@ -22,11 +22,12 @@ def template_matching_ssd(src, temp):
 
     return (pt[1], pt[0])
 
+
 def main():
     # 入力画像とテンプレート画像をで取得
     img = cv2.imread("input.png")
     temp = cv2.imread("temp.png")
-
+    img2 = img.copy()
     # グレースケール変換
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)   
     temp = cv2.cvtColor(temp, cv2.COLOR_RGB2GRAY)   
@@ -35,10 +36,10 @@ def main():
     h, w = temp.shape
 
     # テンプレートマッチング（NumPyで実装）
-    pt = template_matching_ssd(gray, temp)
+    pt = template_matching_sad(gray, temp)
 
     # テンプレートマッチング（OpenCVで実装）
-    #match = cv2.matchTemplate(gray, temp, cv2.TM_SQDIFF)
+    #match = cv2.matchTemplate(gray, temp, cv2.TM_SQDIFF_NORMED)
     #min_value, max_value, pt, size = cv2.minMaxLoc(match)
 
     # テンプレートマッチングの結果を出力
