36303ef79e8aa7101671474311c0575ea8b96843
PrinzEugen <nsi05162@pooae.com>
2017-08-07 19:01:17 +0900

    Create roi.py

---
+++ Python/opencv/image/file/roi.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+import cv2
+import numpy as np
+
+def main():
+    # 入力画像とテンプレート画像をで取得
+    img = cv2.imread("input.png")
+
+    # 窓画像の左上座標
+    x, y = 50, 100
+
+    # 窓画像の幅・高さ
+    w, h = 40, 40
+
+    # 入力画像から窓画像を切り取り
+    roi = src[y:y+h, x:x+w]
+
+    # 窓画像の保存
+    cv2.imwrite("output.png", roi)
+
+
+if __name__ == "__main__":
+    main()
