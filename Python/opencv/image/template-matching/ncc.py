ed231cc944d8ba0b128a637124e0a25e05da2c6c
PrinzEugen <nsi05162@pooae.com>
2017-08-07 16:42:34 +0900

    Update ncc.py

--- Python/opencv/image/template-matching/ncc.py
+++ Python/opencv/image/template-matching/ncc.py
@@ -17,27 +17,37 @@ def template_matching_ncc(src, temp):
             den = np.sqrt(np.sum(src) ** 2) / np.sqrt(np.sum(temp) ** 2)
             if den == 0: score[dy, dx] = 0
             score[dy, dx] = num / den
-        
+
     # スコアが最小の走査位置を返す
-    return np.unravel_index(score.argmin(), score.shape)
+    pt = np.unravel_index(score.argmin(), score.shape)
+
+    return (pt[1], pt[0])
 
 
 def main():
-    # 入力画像とテンプレート画像をグレースケールで取得
+    # 入力画像とテンプレート画像をで取得
     img = cv2.imread("input.png")
     temp = cv2.imread("temp.png")
-
+    img2 = img.copy()
     # グレースケール変換
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)   
     temp = cv2.cvtColor(temp, cv2.COLOR_RGB2GRAY)   
 
-    # テンプレートマッチング（評価値NCC）
-    point = template_matching_ncc(gray, temp)
-    
-    # テンプレートマッチングの結果を出力
-    cv2.rectangle(img, (point[1], point[0] ), (point[1] + temp.shape[0], point[0] + temp.shape[1]), (0,0,200), 3)
-    cv2.imwrite("result.png", img)
+    # テンプレート画像の高さ・幅
+    h, w = temp.shape
 
-    
+    # テンプレートマッチング（NumPyで実装）
+    pt = template_matching_ncc(gray, temp)
+
+    # テンプレートマッチング（OpenCVで実装）
+    #match = cv2.matchTemplate(gray, temp, cv2.TM_CCORR_NORMED)
+    #min_value, max_value, min_pt, max_pt = cv2.minMaxLoc(match)
+    #pt = max_pt
+
+    # テンプレートマッチングの結果を出力
+    cv2.rectangle(img, (pt[0], pt[1] ), (pt[0] + w, pt[1] + h), (0,0,200), 3)
+    cv2.imwrite("output.png", img)
+
+
 if __name__ == "__main__":
     main()
