b2a98b39e871fbf2aeb2c46f74f6c5490a28965e
PrinzEugen <nsi05162@pooae.com>
2017-08-08 16:02:53 +0900

    Update color_tracking.py

--- Python/opencv/camera/moving-object-detection/color_tracking.py
+++ Python/opencv/camera/moving-object-detection/color_tracking.py
@@ -4,22 +4,24 @@ import numpy as np
 
 
 def main():
+    # 赤色のHSVの値域を指定
+    hsv_min = np.array([20,128,0])
+    hsv_max = np.array([200,255,255])
     
     # カメラのキャプチャ
     cap = cv2.VideoCapture(0)
-
+    
     while(cap.isOpened()):
-
+        # フレームを取得
         ret, frame = cap.read()
+        
+        # HSV色空間に変換
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # 緑色(HSV)の範囲を定義
-        hsv_min = np.array([20,128,0])
-        hsv_max = np.array([200,255,255])
-
-        # マスク画像を用いて元画像から指定した色を抽出
+        # 赤色領域のみを取り出してマスク画像を出力
         mask = cv2.inRange(hsv, hsv_min, hsv_max)
-
+        
+        # 結果表示
         cv2.imshow("Frame", frame)
         cv2.imshow("Mask", mask)
 
