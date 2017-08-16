d68cf1cca9abd621ce18bf82fc3a6309ce71b522
PrinzEugen <nsi05162@pooae.com>
2017-08-16 13:47:50 +0900

    Update manga1.py

--- Python/opencv/image/work/manga/manga1.py
+++ Python/opencv/image/work/manga/manga1.py
@@ -3,25 +3,30 @@ import cv2
 import numpy as np
 
 def main():
-    th1, th2 = 60, 160                      # 3値化の閾値
-    gray = cv2.imread("test.png",0)         # 入力画像をグレースケールで取得
-    screen = cv2.imread("screen.png",0)     # スクリーントーンの取得
-    screen = cv2.resize(screen,(gray.shape[1],gray.shape[0]))   # スクリーントーン画像を入力画像と同じ大きさにリサイズ
-    # エッジ検出と色反転
+    # 三値化の閾値
+    th1, th2 = 60, 150 
+
+    # 入力画像とスクリーントーン画像をグレースケールで取得
+    gray = cv2.imread("input.jpg", 0) 
+    screen = cv2.imread("screen.jpg", 0)
+
+    # スクリーントーン画像を入力画像と同じ大きさにリサイズ
+    screen = cv2.resize(screen,(gray.shape[1],gray.shape[0]))
+
+    # Cannyアルゴリズムで輪郭検出し、色反転
     edge = 255 - cv2.Canny(gray, 80, 120)
 
-    # 画像の3値化
+    # 三値化
     gray[gray <= th1] = 0
     gray[gray >= th2] = 255
-    gray[ np.where((gray > th1)&(gray < th2)) ] = screen[ np.where((gray > th1)&(gray < th2)) ]
+    gray[ np.where((gray > th1) & (gray < th2)) ] = screen[ np.where((gray > th1)&(gray < th2)) ]
 
-    # 3値画像とエッジ画像を合わせる
+    # 三値画像と輪郭画像を合成
     gray = cv2.bitwise_and(gray, edge)
 
-    cv2.imshow("Manga",gray)                # 結果を表示
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # 結果を出力
+    cv2.imwrite("output.jpg", gray)
 
 
 if __name__ == '__main__':
-main()
+    main()
